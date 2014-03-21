43e457387911be9c2dfadfefd69a692544c61f20
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-21 08:09:51 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,123 @@
-flashlight
-==========
+## flashlight
 
-Lightweight web proxy client written in go
+Lightweight chained web proxy client written in go.
+
+flashlight runs in one of two modes:
+
+client - meant to run locally to wherever the browser is running, forwards requests to the server
+server - handles requests from an upstream flashlight client proxy and actually proxies them to the final destination
+
+Using CloudFront, flashlight has the ability to masquerade as running on a different domain than it is.  The client simply
+specified the "masquerade" flag with a value like "thehackernews.com".  flashlight will then use that masquerade host for
+the DNS lookup and will also specify it as the ServerName for SNI (though this is not actually necessary on CloudFront).
+The Host header of the HTTP request will actually contain the correct host (e.g. getiantem.org), which causes CloudFront
+to route the request to the correct host.
+
+### Usage
+
+Usage of flashlight:
+  -addr="": ip:port on which to listen for requests.  When running as a client proxy, we'll listen with http, when running as a server proxy we'll listen with https
+  -help=false: Get usage help
+  -masquerade="": masquerade host: if specified, flashlight will actually make a request to this host's IP but with a host header corresponding to the 'server' parameter
+  -server="": hostname at which to connect to a server flashlight (always using https).  When specified, this flashlight will run as a client proxy, otherwise it runs as a server
+  -serverPort=443: the port on which to connect to the server
+
+###
+
+Example Server:
+
+```bash
+./flashlight -addr 0.0.0.0:443
+```
+
+Example Client:
+
+```bash
+./flashlight -addr localhost:10080 -server getiantem.org -masquerade thehackernews.com
+```
+
+Example Curl Test:
+
+```bash
+curl -x localhost:10080 http://www.google.com/humans.txt
+Google is built by a large team of engineers, designers, researchers, robots, and others in many different sites across the globe. It is updated continuously, and built with more tools and technologies than we can shake a stick at. If you'd like to help us out, see google.com/careers.
+```
+
+On the server, you should see something like this for every request that's received:
+```bash
+2014/03/21 09:08:30 Request: (*http.Request)(0xc2100a98f0)({
+ Method: (string) "GET",
+ URL: (*url.URL)(0xc21006d1c0)(/humans.txt),
+ Proto: (string) "HTTP/1.1",
+ ProtoMajor: (int) 1,
+ ProtoMinor: (int) 1,
+ Header: (http.Header) {
+  (string) "Cf-Ipcountry": ([]string) {
+   (string) "US"
+  },
+  (string) "X-Forwarded-For": ([]string) {
+   (string) "127.0.0.1,66.69.242.177"
+  },
+  (string) "User-Agent": ([]string) {
+   (string) "curl/7.30.0"
+  },
+  (string) "Connection": ([]string) {
+   (string) "Keep-Alive"
+  },
+  (string) "Accept-Encoding": ([]string) {
+   (string) "gzip"
+  },
+  (string) "Cf-Connecting-Ip": ([]string) {
+   (string) "66.69.242.177"
+  },
+  (string) "Cf-Ray": ([]string) {
+   (string) "10ea60e8aec90956-DFW"
+  },
+  (string) "X-Forwarded-Proto": ([]string) {
+   (string) "https"
+  },
+  (string) "Cf-Visitor": ([]string) {
+   (string) "{\"scheme\":\"https\"}"
+  },
+  (string) "Accept": ([]string) {
+   (string) "*/*"
+  },
+  (string) "X-Lantern-Host": ([]string) {
+   (string) "www.google.com"
+  }
+ },
+ Body: (*struct { *strings.Reader; io.Closer })(0x90ced0)({
+  Reader: (*strings.Reader)(0xc21004d240)({
+   s: (string) "",
+   i: (int) 0,
+   prevRune: (int) -1
+  }),
+  Closer: (ioutil.nopCloser) {
+   Reader: (io.Reader) <nil>
+  }
+ }),
+ ContentLength: (int64) 0,
+ TransferEncoding: ([]string) <nil>,
+ Close: (bool) false,
+ Host: (string) "getiantem.org",
+ Form: (url.Values) {
+ },
+ PostForm: (url.Values) {
+ },
+ MultipartForm: (*multipart.Form)(<nil>),
+ Trailer: (http.Header) {
+ },
+ RemoteAddr: (string) "173.245.50.227:23690",
+ RequestURI: (string) "/humans.txt",
+ TLS: (*tls.ConnectionState)(0xc210052cc0)({
+  HandshakeComplete: (bool) true,
+  DidResume: (bool) false,
+  CipherSuite: (uint16) 49199,
+  NegotiatedProtocol: (string) "",
+  NegotiatedProtocolIsMutual: (bool) true,
+  ServerName: (string) "getiantem.org",
+  PeerCertificates: ([]*x509.Certificate) <nil>,
+  VerifiedChains: ([][]*x509.Certificate) <nil>
+ })
+})
+```
---
+++ flashlight.go
@@ -0,0 +1,195 @@
+// flashlight is a lightweight proxy that can be run in client or server mode.
+package main
+
+import (
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"flag"
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/oxtoacart/keyman"
+	"log"
+	"math/big"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"sync"
+	"time"
+)
+
+const (
+	CONNECT        = "CONNECT"
+	ONE_WEEK       = 7 * 24 * time.Hour
+	TWO_WEEKS      = ONE_WEEK * 2
+	X_LANTERN_HOST = "X-Lantern-Host"
+
+	PK_FILE   = "proxypk.pem"
+	CERT_FILE = "proxycert.pem"
+)
+
+var (
+	help         = flag.Bool("help", false, "Get usage help")
+	addr         = flag.String("addr", "", "ip:port on which to listen for requests.  When running as a client proxy, we'll listen with http, when running as a server proxy we'll listen with https")
+	upstreamHost = flag.String("server", "", "hostname at which to connect to a server flashlight (always using https).  When specified, this flashlight will run as a client proxy, otherwise it runs as a server")
+	upstreamPort = flag.Int("serverPort", 443, "the port on which to connect to the server")
+	masqueradeAs = flag.String("masquerade", "", "masquerade host: if specified, flashlight will actually make a request to this host's IP but with a host header corresponding to the 'server' parameter")
+
+	isDownstream bool
+
+	pk             *keyman.PrivateKey
+	pkPem          []byte
+	issuingCert    *keyman.Certificate
+	issuingCertPem []byte
+
+	wg sync.WaitGroup
+)
+
+func init() {
+	flag.Parse()
+	if *help || *addr == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	isDownstream = *upstreamHost != ""
+}
+
+func main() {
+	if isDownstream {
+		runClient()
+	} else {
+		if err := initCerts(); err != nil {
+			log.Fatalf("Unable to initialize certs: %s", err)
+		}
+		runServer()
+	}
+	wg.Wait()
+}
+
+func runClient() {
+	wg.Add(1)
+
+	server := &http.Server{
+		Addr:         *addr,
+		Handler:      http.HandlerFunc(handleClient),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		log.Printf("About to start client (http) proxy at %s", *addr)
+		if err := server.ListenAndServe(); err != nil {
+			log.Fatalf("Unable to start client proxy: %s", err)
+		}
+		wg.Done()
+	}()
+}
+
+func runServer() {
+	wg.Add(1)
+
+	server := &http.Server{
+		Addr:         *addr,
+		Handler:      http.HandlerFunc(handleServer),
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	go func() {
+		log.Printf("About to start server (https) proxy at %s", *addr)
+		if err := server.ListenAndServeTLS(CERT_FILE, PK_FILE); err != nil {
+			log.Fatalf("Unable to start server proxy: %s", err)
+		}
+		wg.Done()
+	}()
+}
+
+// handleClient handles requests from a local client (e.g. the browser)
+func handleClient(resp http.ResponseWriter, req *http.Request) {
+	host := *upstreamHost
+	if *masqueradeAs != "" {
+		host = *masqueradeAs
+	}
+	upstreamAddr := fmt.Sprintf("%s:%d", host, *upstreamPort)
+
+	rp := httputil.ReverseProxy{
+		Director: func(req *http.Request) {
+			// Remember the host that was actually requested
+			req.Header.Set(X_LANTERN_HOST, req.Host)
+			// Set our upstream proxy as the host for this request
+			req.Host = *upstreamHost
+			req.URL.Host = *upstreamHost
+		},
+		Transport: &http.Transport{
+			Dial: func(network, addr string) (net.Conn, error) {
+				tlsConfig := &tls.Config{
+					ServerName: host,
+				}
+				log.Printf("Dialing %s", upstreamAddr)
+				return tls.Dial(network, upstreamAddr, tlsConfig)
+			},
+		},
+	}
+	rp.ServeHTTP(resp, req)
+}
+
+// handleServer handles requests from a downstream flashlight
+func handleServer(resp http.ResponseWriter, req *http.Request) {
+	rp := httputil.ReverseProxy{
+		Director: func(req *http.Request) {
+			log.Printf("Request: %s", spew.Sdump(req))
+			// TODO - need to add support for tunneling HTTPS traffic using CONNECT
+			req.URL.Scheme = "http"
+			// Grab the actual host from the original client and use that for the outbound request
+			req.URL.Host = req.Header.Get(X_LANTERN_HOST)
+		},
+	}
+	rp.ServeHTTP(resp, req)
+}
+
+func initCerts() (err error) {
+	if pk, err = keyman.LoadPKFromFile(PK_FILE); err != nil {
+		if pk, err = keyman.GeneratePK(2048); err != nil {
+			return
+		}
+		if err = pk.WriteToFile(PK_FILE); err != nil {
+			return
+		}
+	}
+	pkPem = pk.PEMEncoded()
+	if issuingCert, err = keyman.LoadCertificateFromFile(CERT_FILE); err != nil {
+		if issuingCert, err = certificateFor("Lantern", nil); err != nil {
+			return
+		}
+		if err = issuingCert.WriteToFile(CERT_FILE); err != nil {
+			return
+		}
+	}
+	issuingCertPem = issuingCert.PEMEncoded()
+	return
+}
+
+func certificateFor(name string, issuer *keyman.Certificate) (cert *keyman.Certificate, err error) {
+	now := time.Now()
+	template := &x509.Certificate{
+		SerialNumber: new(big.Int).SetInt64(int64(time.Now().Nanosecond())),
+		Subject: pkix.Name{
+			Organization: []string{"Lantern"},
+			CommonName:   name,
+		},
+		NotBefore: now.Add(-1 * ONE_WEEK),
+		NotAfter:  now.Add(TWO_WEEKS),
+
+		BasicConstraintsValid: true,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+	}
+	if issuer == nil {
+		template.KeyUsage = template.KeyUsage | x509.KeyUsageCertSign
+		template.ExtKeyUsage = []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth}
+		template.IsCA = true
+	}
+	cert, err = pk.Certificate(template, issuer)
+	return
+}
