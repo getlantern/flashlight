ceced24d1fb5657a4a5fdc9a3ebb18d7463173db
Ox To A Cart <ox.to.a.cart@gmail.com>
2014-03-24 16:51:37 -0500

    In the middle of playing with things

--- flashlight.go
+++ flashlight.go
@@ -2,12 +2,12 @@
 package main
 
 import (
-	"crypto/tls"
+	//"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"flag"
 	"fmt"
-	"github.com/oxtoacart/keyman"
+	"io"
 	"log"
 	"math/big"
 	"net"
@@ -15,15 +15,21 @@ import (
 	"net/http/httputil"
 	"os"
 	"runtime"
+	"strings"
 	"sync"
 	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/oxtoacart/keyman"
 )
 
 const (
-	CONNECT        = "CONNECT"
-	ONE_WEEK       = 7 * 24 * time.Hour
-	TWO_WEEKS      = ONE_WEEK * 2
-	X_LANTERN_HOST = "X-Lantern-Host"
+	CONNECT   = "CONNECT"
+	ONE_WEEK  = 7 * 24 * time.Hour
+	TWO_WEEKS = ONE_WEEK * 2
+
+	X_LANTERN_HOST   = "X-Lantern-Host"
+	X_LANTERN_METHOD = "X-Lantern-Method"
 
 	PK_FILE   = "proxypk.pem"
 	CERT_FILE = "proxycert.pem"
@@ -33,7 +39,7 @@ var (
 	help         = flag.Bool("help", false, "Get usage help")
 	addr         = flag.String("addr", "", "ip:port on which to listen for requests.  When running as a client proxy, we'll listen with http, when running as a server proxy we'll listen with https")
 	upstreamHost = flag.String("server", "", "hostname at which to connect to a server flashlight (always using https).  When specified, this flashlight will run as a client proxy, otherwise it runs as a server")
-	upstreamPort = flag.Int("serverPort", 443, "the port on which to connect to the server")
+	upstreamPort = flag.Int("serverPort", 80, "the port on which to connect to the server")
 	masqueradeAs = flag.String("masquerade", "", "masquerade host: if specified, flashlight will actually make a request to this host's IP but with a host header corresponding to the 'server' parameter")
 
 	isDownstream bool
@@ -101,7 +107,8 @@ func runServer() {
 
 	go func() {
 		log.Printf("About to start server (https) proxy at %s", *addr)
-		if err := server.ListenAndServeTLS(CERT_FILE, PK_FILE); err != nil {
+		// if err := server.ListenAndServeTLS(CERT_FILE, PK_FILE); err != nil {
+		if err := server.ListenAndServe(); err != nil {
 			log.Fatalf("Unable to start server proxy: %s", err)
 		}
 		wg.Done()
@@ -110,48 +117,156 @@ func runServer() {
 
 // handleClient handles requests from a local client (e.g. the browser)
 func handleClient(resp http.ResponseWriter, req *http.Request) {
+	log.Println(spew.Sdump(req))
+
 	host := *upstreamHost
 	if *masqueradeAs != "" {
 		host = *masqueradeAs
 	}
 	upstreamAddr := fmt.Sprintf("%s:%d", host, *upstreamPort)
 
+	// Remember the host that was actually requested
+	req.Header.Set(X_LANTERN_HOST, req.Host)
+	// Set our upstream proxy as the host for this request
+	req.Host = *upstreamHost
+	// TODO: handle https too
+	req.URL.Scheme = "http"
+	req.Header.Set("Host", *upstreamHost)
+	req.URL.Host = *upstreamHost
+	// To allow tunneling through reverse proxies that don't support CONNECT,
+	// pretend to be a GET request.
+	if req.Method == "CONNECT" {
+		req.Method = "POST"
+		req.Header.Set(X_LANTERN_METHOD, "CONNECT")
+	}
+
+	log.Printf("Using %s to handle request for: %s", upstreamAddr, req.URL.String())
+	if connIn, _, err := resp.(http.Hijacker).Hijack(); err != nil {
+		msg := fmt.Sprintf("Unable to access underlying connection from client: %s", err)
+		respondBadGateway(resp, req, msg)
+	} else {
+		// serverIsLocalhost := strings.Contains(*addr, "localhost") || strings.Contains(*addr, "127.0.0.1")
+		// tlsConfig := &tls.Config{
+		// 	ServerName:         host,
+		// 	InsecureSkipVerify: serverIsLocalhost,
+		// }
+		// connOut, err := tls.Dial("tcp", upstreamAddr, tlsConfig)
+		connOut, err := net.Dial("tcp", upstreamAddr)
+		if err != nil {
+			if connOut != nil {
+				defer connOut.Close()
+			}
+			msg := fmt.Sprintf("Unable to dial server: %s", err)
+			log.Println(msg)
+			respondBadGateway(resp, req, msg)
+		} else {
+			err := writeRequest(req, connOut)
+			if err != nil {
+				msg := fmt.Sprintf("Unable to write request to server: %s", err)
+				respondBadGateway(resp, req, msg)
+			} else {
+				log.Println("Wrote request, piping traffic")
+				go func() {
+					pipe(connIn, connOut)
+					log.Println("Done piping out")
+				}()
+			}
+		}
+	}
+}
+
+// handleServer handles requests from a downstream flashlight
+func handleServer(resp http.ResponseWriter, req *http.Request) {
+	// Grab the actual host from the original client and use that for the outbound request
+	req.URL.Host = req.Header.Get(X_LANTERN_HOST)
+	req.Host = req.URL.Host
+	if "CONNECT" == req.Header.Get(X_LANTERN_METHOD) {
+		// Request is actually a CONNECT pretending to be a POST
+		handleServerHTTPS(resp, req)
+	} else {
+		handleServerHTTP(resp, req)
+	}
+}
+
+// handleServerHTTP handles http requests from a downstream flashlight
+func handleServerHTTP(resp http.ResponseWriter, req *http.Request) {
 	rp := httputil.ReverseProxy{
 		Director: func(req *http.Request) {
-			// Remember the host that was actually requested
-			req.Header.Set(X_LANTERN_HOST, req.Host)
-			// Set our upstream proxy as the host for this request
-			req.Host = *upstreamHost
-			// TODO: handle https too
 			req.URL.Scheme = "http"
-			req.URL.Host = *upstreamHost
-		},
-		Transport: &http.Transport{
-			Dial: func(network, addr string) (net.Conn, error) {
-				tlsConfig := &tls.Config{
-					ServerName: host,
-				}
-				log.Printf("Using %s to handle request for: %s", upstreamAddr, req.URL.String())
-				return tls.Dial(network, upstreamAddr, tlsConfig)
-			},
+			log.Printf("Handling request for: %s", req.URL.String())
 		},
 	}
 	rp.ServeHTTP(resp, req)
 }
 
-// handleServer handles requests from a downstream flashlight
-func handleServer(resp http.ResponseWriter, req *http.Request) {
-	rp := httputil.ReverseProxy{
-		Director: func(req *http.Request) {
-			// TODO - need to add support for tunneling HTTPS traffic using CONNECT
-			req.URL.Scheme = "http"
-			// Grab the actual host from the original client and use that for the outbound request
-			req.URL.Host = req.Header.Get(X_LANTERN_HOST)
-			req.Host = req.URL.Host
-			log.Printf("Handling request for: %s", req.URL.String())
-		},
+// handleServerHTTPS handles HTTPS requests from a downstream flashlight
+func handleServerHTTPS(resp http.ResponseWriter, req *http.Request) {
+	if connIn, _, err := resp.(http.Hijacker).Hijack(); err != nil {
+		msg := fmt.Sprintf("Unable to access underlying connection from client: %s", err)
+		respondBadGateway(resp, req, msg)
+	} else {
+		connOut, err := net.Dial("tcp", hostIncludingPort(req))
+		if err != nil {
+			if connOut != nil {
+				defer connOut.Close()
+			}
+			msg := fmt.Sprintf("Unable to dial server: %s", err)
+			log.Println(msg)
+			respondBadGateway(resp, req, msg)
+		} else {
+			log.Printf("Tunneling traffic")
+			pipe(connIn, connOut)
+			connIn.Write([]byte("HTTP/1.1 200 OK\r\nTransfer-Encoding: Chunked\r\n\r\n"))
+		}
 	}
-	rp.ServeHTTP(resp, req)
+}
+
+func writeRequest(req *http.Request, conn io.Writer) error {
+	if _, err := fmt.Fprintf(conn, "%s %s HTTP/1.1\r\n", valueOrDefault(req.Method, "GET"), req.URL.RequestURI()); err != nil {
+		return err
+	}
+	if err := req.Header.Write(conn); err != nil {
+		return err
+	}
+	_, err := io.WriteString(conn, "\r\n")
+	return err
+}
+
+func valueOrDefault(value, def string) string {
+	if value != "" {
+		return value
+	}
+	return def
+}
+
+func hostIncludingPort(req *http.Request) (host string) {
+	host = req.Host
+	if !strings.Contains(host, ":") {
+		if req.Method == "CONNECT" {
+			host = host + ":443"
+		} else {
+			host = host + ":80"
+		}
+	}
+	return
+}
+
+func respondBadGateway(resp http.ResponseWriter, req *http.Request, msg string) {
+	resp.WriteHeader(502)
+	resp.Write([]byte(fmt.Sprintf("Bad Gateway: %s - %s", req.URL, msg)))
+}
+
+func pipe(connIn net.Conn, connOut net.Conn) {
+	go func() {
+		defer connIn.Close()
+		io.Copy(connOut, connIn)
+		log.Println("Done piping out")
+	}()
+	go func() {
+		defer connOut.Close()
+		io.Copy(connIn, connOut)
+		log.Println("Done piping in")
+	}()
 }
 
 func initCerts() (err error) {
@@ -165,7 +280,7 @@ func initCerts() (err error) {
 	}
 	pkPem = pk.PEMEncoded()
 	if issuingCert, err = keyman.LoadCertificateFromFile(CERT_FILE); err != nil {
-		if issuingCert, err = certificateFor("Lantern", nil); err != nil {
+		if issuingCert, err = certificateFor("localhost", nil); err != nil {
 			return
 		}
 		if err = issuingCert.WriteToFile(CERT_FILE); err != nil {
