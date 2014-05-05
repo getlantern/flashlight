659686d28028ede665b8f21a9bb866224328654d
Ox Cart <ox.to.a.cart@gmail.com>
2014-05-05 10:54:11 -0500

    Organized protocols into sub-packages

---
+++ protocol/fastly/common.go
@@ -0,0 +1,9 @@
+// package fastly provides an implementation of a host-spoofing protocol for
+// Fastly.
+package fastly
+
+const (
+	CF_PREFIX        = "Cf-"
+	X_LANTERN_PREFIX = "X-Lantern-"
+	X_LANTERN_URL    = X_LANTERN_PREFIX + "URL"
+)
