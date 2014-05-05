8a2de281dfeb9b4feded9052c69038eeec2a3278
Ox Cart <ox.to.a.cart@gmail.com>
2014-05-05 07:32:16 -0500

    TLS improvements on server side

--- flashlight.go
+++ flashlight.go
@@ -260,6 +260,9 @@ func runServer() {
 			Certificates: []tls.Certificate{cert},
 			CipherSuites: []uint16{
 				tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+				tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
 			},
 		},
 	}
