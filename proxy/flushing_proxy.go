4a4c425bf561192732857c90445382389c71b6e0
Ox Cart <ox.to.a.cart@gmail.com>
2014-05-19 09:32:51 -0500

    Forcing responses to get flushed every second

---
+++ proxy/flushing_proxy.go
@@ -0,0 +1,48 @@
+package proxy
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httputil"
+	"strings"
+	"time"
+)
+
+const (
+	X_LANTERN_STREAMING = "X-Lantern-Streaming"
+	YES                 = "yes"
+)
+
+// flushingReverseProxy is a ReverseProxy that will periodically flush responses
+// for any request that contained the header "X-Lantern-Streaming" set to "yes".
+type flushingReverseProxy struct {
+	reverseProxy             *httputil.ReverseProxy // the main reverse proxy
+	reverseProxyWithFlushing *httputil.ReverseProxy // a copy of the main reverse proxy that flushes at regular intervals
+}
+
+func newFlushingRevereseProxy(rp *httputil.ReverseProxy, flushInterval time.Duration) (*flushingReverseProxy, error) {
+	// Make a copy of the rp, using the given flushInterval
+	frp := &httputil.ReverseProxy{
+		Director:      rp.Director,
+		FlushInterval: flushInterval,
+	}
+	if rp.Transport != nil {
+		tr, ok := rp.Transport.(*http.Transport)
+		if !ok {
+			return nil, fmt.Errorf("newFlushingReverseProxy only works for proxies whose RoundTripper is the default http.Transport")
+		}
+		frp.Transport = &(*tr)
+	}
+	return &flushingReverseProxy{
+		reverseProxy:             rp,
+		reverseProxyWithFlushing: frp,
+	}, nil
+}
+
+func (rp *flushingReverseProxy) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
+	if strings.ToLower(req.Header.Get(X_LANTERN_STREAMING)) == YES {
+		rp.reverseProxyWithFlushing.ServeHTTP(resp, req)
+	} else {
+		rp.reverseProxy.ServeHTTP(resp, req)
+	}
+}
