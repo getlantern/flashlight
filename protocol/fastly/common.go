0ab9f6507787c8b7ca596040dfb7bb13d86a5423
Ox Cart <ox.to.a.cart@gmail.com>
2014-06-02 10:45:21 -0500

    Removed protocols

--- protocol/fastly/common.go
+++
@@ -1,9 +0,0 @@
-// package fastly provides an implementation of a host-spoofing protocol for
-// Fastly.
-package fastly
-
-const (
-	CF_PREFIX        = "Cf-"
-	X_LANTERN_PREFIX = "X-Lantern-"
-	X_LANTERN_URL    = X_LANTERN_PREFIX + "URL"
-)
