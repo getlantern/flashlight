602563205f2a382a01aa2cbeacdf349264d6f398
Myles Horton <myles@getlantern.org>
2014-08-04 13:18:45 -0700

    cleanups and code signing

--- copyexecutables.bash
+++ copyexecutables.bash
@@ -5,8 +5,13 @@ function die() {
   exit 1
 }
 
+# Sign while we're at it...
+
+xcbase=$GOPATH/bin/flashlight-xc/snapshot
 lantern=$HOME/lantern/install
-cp $GOPATH/bin/flashlight-xc/snapshot/darwin_amd64/flashlight $lantern/osx/pt/flashlight/ || die "Could not copy darwin"
-cp $GOPATH/bin/flashlight-xc/snapshot/windows_386/flashlight.exe $lantern/win/pt/flashlight/ || die "Could not copy windows"
-cp $GOPATH/bin/flashlight-xc/snapshot/linux_386/flashlight $lantern/linux_x86_32/pt/flashlight/ || die "Could not copy linux 32"
-cp $GOPATH/bin/flashlight-xc/snapshot/linux_amd64/flashlight $lantern/linux_x86_64/pt/flashlight/ || die "Could not copy linux 64"
+codesign -s "Developer ID Application: Brave New Software Project, Inc" -f $xcbase/darwin_amd64/flashlight || die "Could not code sign?"
+
+cp $xcbase/darwin_amd64/flashlight $lantern/osx/pt/flashlight/ || die "Could not copy darwin"
+cp $xcbase/windows_386/flashlight.exe $lantern/win/pt/flashlight/ || die "Could not copy windows"
+cp $xcbase/linux_386/flashlight $lantern/linux_x86_32/pt/flashlight/ || die "Could not copy linux 32"
+cp $xcbase/linux_amd64/flashlight $lantern/linux_x86_64/pt/flashlight/ || die "Could not copy linux 64"
