cc40bc57c7910f9077a8240aa7db7a21789d080a
myleshorton <myles@getlantern.org>
2014-08-18 14:16:29 -0700

    Added prerequisites

--- uploadghasset.rb
+++ uploadghasset.rb
@@ -1,6 +1,11 @@
 #!/usr/bin/env ruby
 
 # This allows us to easily deploy flashlight binaries by tag to GitHub, for example:
+# Prerequisites:
+#
+# 1) gem install octokit
+# 2) Obtain an oauth token at https://help.github.com/articles/creating-an-access-token-for-command-line-use
+# 3) Set your GH_TOKEN environment variable to that token
 #
 # ./uploadghasset.rb 0.0.1 ../../../../bin/flashlight-xc/snapshot/linux_amd64/flashlight 
 require 'octokit'
