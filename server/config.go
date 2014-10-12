320cc001d697fe431e06a5d4899ed4216d6722cb
Todd Blose <todd@lana.io>
2014-10-11 19:52:47 -0700

    remove redundant active peers map

--- server/config.go
+++ server/config.go
@@ -3,4 +3,5 @@ package server
 type ServerConfig struct {
 	Portmap        int
 	AdvertisedHost string // FQDN that is guaranteed to hit this server
+	WaddellAddr    string // Address at which to connect to waddell for signaling
 }
