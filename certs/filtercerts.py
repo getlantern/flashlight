466f7a0a48bdccf4307f9693b4adf10e50fb8c22
Alan Hoyte <aranhoide@gmail.com>
2014-11-03 10:06:30 +0100

    remove cert files and other obsolete stuff
    The cert files are replaced by cloud.yaml, which acquires authoritative status.
    The Python scripts are integrated into a new one, which will be comitted
    separately for easier review.

--- certs/filtercerts.py
+++
@@ -1,6 +0,0 @@
-_noncert_extensions = [".yaml", ".txt", ".py", ".pyc", ".tmpl", ".bash",
-		               ".swp", ".swo"]
-
-def iscert(filename):
-	extension = filename[filename.rfind('.'):]
-	return extension not in _noncert_extensions
