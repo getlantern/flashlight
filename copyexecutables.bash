286ea34274fa8cbc5a7aae1f7734b299687b9017
Ox Cart <ox.to.a.cart@gmail.com>
2014-11-12 09:13:59 -0600

    Added ability to sign windows executable on copyexecutables

--- copyexecutables.bash
+++ copyexecutables.bash
@@ -10,15 +10,29 @@ then
     die "$0: Path to lantern required"
 fi
 
+if [ ! -z "$BNS_CERT" ]
+then
+	if [ -z "$BNS_CERT_PASS" ]
+	then
+		die "$0: Please set BNS_CERT_PASS to the password for the $BNS_CERT signing key"
+	fi
+fi
+
 # Sign while we're at it...
 
-xcbase=$GOPATH/bin/flashlight-xc/snapshot
 lantern=$1/install
-codesign -s "Developer ID Application: Brave New Software Project, Inc" -f $xcbase/darwin_amd64/flashlight || die "Could not code sign?"
+codesign -s "Developer ID Application: Brave New Software Project, Inc" -f flashlight_darwin_amd64 || die "Could not code sign?"
 
 echo "Copying executables to $1"
 
-cp $xcbase/darwin_amd64/flashlight $lantern/osx/pt/flashlight/ || die "Could not copy darwin"
-cp $xcbase/windows_386/flashlight.exe $lantern/win/pt/flashlight/ || die "Could not copy windows"
-cp $xcbase/linux_386/flashlight $lantern/linux_x86_32/pt/flashlight/ || die "Could not copy linux 32"
-cp $xcbase/linux_amd64/flashlight $lantern/linux_x86_64/pt/flashlight/ || die "Could not copy linux 64"
+cp flashlight_darwin_amd64 $lantern/osx/pt/flashlight/ || die "Could not copy darwin"
+if [ -z "$BNS_CERT" ]
+then
+	echo "WARNING - No BNS_CERT set, simply copying windows executable"
+	cp flashlight_windows_386.exe $lantern/win/pt/flashlight/ || die "Could not copy windows"
+else
+	echo "Signing windows executable"
+	osslsigncode sign -pkcs12 "$BNS_CERT" -pass "$BNS_CERT_PASS" -in flashlight_windows_386.exe -out $lantern/win/pt/flashlight/flashlight.exe || die "Could not sign windows"
+fi
+cp flashlight_linux_386 $lantern/linux_x86_32/pt/flashlight/ || die "Could not copy linux 32"
+cp flashlight_linux_amd64 $lantern/linux_x86_64/pt/flashlight/ || die "Could not copy linux 64"
--- crosscompile.bash
+++ crosscompile.bash
@@ -2,4 +2,4 @@
 
 VERSION="`git describe --abbrev=0 --tags`"
 BUILD_DATE="`date -u +%Y%m%d%.%H%M%S`"
-goxc -build-ldflags="-w -X main.version $VERSION -X main.buildDate $BUILD_DATE" -bc="linux,386 linux,amd64 windows,386 darwin" validate compile
+gox -ldflags="-w -X main.version $VERSION -X main.buildDate $BUILD_DATE" -osarch="linux/386 linux/amd64 windows/386 darwin/amd64"
