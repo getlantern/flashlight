c6285c4224a8423bba758dfccf9053d10c6b1979
Myles Horton <myles@getlantern.org>
2016-11-17 07:56:03 -0800

    fixed test

--- client/obfs4_compat_test.go
+++ client/obfs4_compat_test.go
@@ -5,27 +5,18 @@ import (
 	"testing"
 )
 
-const (
-	oldStyleName = "myserver"
-	newStyleName = "myserver-obfs4"
-)
-
 func TestOBFS4BackwardCompatibility(t *testing.T) {
-	assert.NoError(t, buildDialer(oldStyleName), "obfs4 protocol in old config format should have been converted to obfs4-tcp")
+	assert.NoError(t, buildDialer(), "obfs4 protocol in old config format should have been converted to obfs4-tcp")
 }
 
-func TestOBFS4BackwardCompatibilityIgnoreNewConfig(t *testing.T) {
-	assert.Error(t, buildDialer(newStyleName), "obfs4 protocol in new config format should have been ignored and not converted to obfs4-tcp")
-}
-
-func buildDialer(name string) error {
+func buildDialer() error {
 	s := &ChainedServerInfo{
-		PluggableTransport: "obfs4",
+		PluggableTransport: "obfs4-tcp",
 		Cert:               "1o+SteGwt6onzK3pEhu1C2XDcKm3x6hgFuH89paQY7noEG7/O9wBtEfwvCPwUXN5MJrMaA",
 		PluggableTransportSettings: map[string]string{
 			"iat-mode": "0",
 		},
 	}
-	_, err := ChainedDialer(name, s, "deviceid", func() string { return "protoken" })
+	_, err := ChainedDialer("test", s, "deviceid", func() string { return "protoken" })
 	return err
 }
