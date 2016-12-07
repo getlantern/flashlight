e592b7224ca433f5f4167924a5475c67c72ef7d1
Ox Cart <ox.to.a.cart@gmail.com>
2016-12-07 12:22:22 -0600

    Added unit test for ops.Request

--- ops/ops.go
+++ ops/ops.go
@@ -95,18 +95,13 @@ func (op *Op) UserAgent(v string) *Op {
 }
 
 // Request attaches key information of an `http.Request` to the Context.
-func (op *Op) Request(r *http.Request) *Op {
-	if r == nil {
+func (op *Op) Request(req *http.Request) *Op {
+	if req == nil {
 		return op
 	}
-	op.Set("http_method", r.Method).
-		Set("http_proto", r.Proto).
-		Set("origin", r.Host)
-	op.Origin(r)
-	host, port, err := net.SplitHostPort(r.Host)
-	if err == nil {
-		op.Set("origin_host", host).Set("origin_port", port)
-	}
+	op.Set("http_method", req.Method).
+		Set("http_proto", req.Proto)
+	op.Origin(req)
 	return op
 }
 
@@ -149,6 +144,7 @@ func (op *Op) ProxyProtocol(v string) *Op {
 
 // Origin attaches the origin to the Contetx
 func (op *Op) Origin(req *http.Request) *Op {
+	op.Set("origin", req.Host)
 	host, port, err := net.SplitHostPort(req.Host)
 	if err != nil {
 		host = req.Host
---
+++ ops/ops_test.go
@@ -0,0 +1,40 @@
+package ops
+
+import (
+	"fmt"
+	"github.com/getlantern/testify/assert"
+	"net/http"
+	"testing"
+)
+
+func TestRequest(t *testing.T) {
+	methods := []string{http.MethodGet, http.MethodPost, http.MethodHead, http.MethodConnect, http.MethodConnect}
+	protos := []string{"http", "http", "https", "http", "http"}
+	hosts := []string{"www.duckduckgo.com", "www.duckduckgo.com:8080", "www.duckduckgo.com:443", "www.duckduckgo.com:443", "www.duckduckgo.com"}
+	expectedPorts := []string{"80", "8080", "443", "443", ""}
+
+	var ctxs []map[string]interface{}
+	RegisterReporter(func(failure error, ctx map[string]interface{}) {
+		ctxs = append(ctxs, ctx)
+	})
+
+	for i, method := range methods {
+		proto := protos[i]
+		host := hosts[i]
+		req, _ := http.NewRequest(method, fmt.Sprintf("%v://%v", proto, host), nil)
+		op := Begin("test").Request(req)
+		op.End()
+	}
+
+	if assert.Len(t, ctxs, len(methods)) {
+		for i, ctx := range ctxs {
+			method := methods[i]
+			host := hosts[i]
+			port := expectedPorts[i]
+			assert.Equal(t, method, ctx["http_method"])
+			assert.Equal(t, "HTTP/1.1", ctx["http_proto"])
+			assert.Equal(t, host, ctx["origin"])
+			assert.Equal(t, port, ctx["origin_port"])
+		}
+	}
+}
