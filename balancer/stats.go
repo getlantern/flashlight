1771d1babbc7a5f617836ebdb5ccba2886b376a0
Ox Cart <ox.to.a.cart@gmail.com>
2017-01-02 07:34:56 -0600

    Formatting improvements based on joesis suggestions

--- balancer/stats.go
+++ balancer/stats.go
@@ -18,5 +18,5 @@ func (s *stats) String(d *dialer) string {
 	failures := atomic.LoadInt64(&s.failures)
 	consecFailures := d.ConsecFailures()
 	attempts := atomic.LoadInt64(&s.attempts)
-	return fmt.Sprintf("Dialer Stats for %v\t\tSuccess: %7d / %7d (%7d)\t\tFailure: %7d / %7d (%7d)\t\tEMA Latency: %v", d.Label, successes, attempts, consecSuccesses, failures, attempts, consecFailures, d.emaLatency.Get())
+	return fmt.Sprintf("%s  S: %4d / %4d (%d)\tF: %4d / %4d (%d)\tL: %3.2fs", d.Label, successes, attempts, consecSuccesses, failures, attempts, consecFailures, d.emaLatency.Get().Seconds())
 }
