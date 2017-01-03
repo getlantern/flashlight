08d896b617d63c3d6032aa235fcdf6c9f3dca241
Ox Cart <ox.to.a.cart@gmail.com>
2017-01-03 14:35:31 -0600

    Moved rate tracking to chained package

--- chained/rater_test.go
+++ chained/rater_test.go
@@ -1,4 +1,4 @@
-package balancer
+package chained
 
 import (
 	"github.com/stretchr/testify/assert"
--- chained/ratetracking.go
+++ chained/ratetracking.go
@@ -1,4 +1,4 @@
-package balancer
+package chained
 
 import (
 	"github.com/getlantern/flashlight/ops"
