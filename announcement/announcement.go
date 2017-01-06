fa4e4f154a24945a504ebe0132bf9146eb0de18b
joesis <joesis@getlantern.org>
2017-01-06 03:22:15 -0800

    finish announcement package

---
+++ announcement/announcement.go
@@ -0,0 +1,158 @@
+package app
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/getlantern/errors"
+	"github.com/getlantern/notifier"
+
+	"github.com/getlantern/flashlight/ui"
+)
+
+const loConfURL = "https://raw.githubusercontent.com/getlantern/loconf/master/desktop-ui.json"
+const stagingLoConfURL = "https://raw.githubusercontent.com/getlantern/loconf/master/ui-staging.json"
+
+var eIncorrectType error = errors.New("Incorrect type")
+var eNoAvailable error = errors.New("No announcement available")
+
+type Announcement struct {
+	Campaign string `json:"campaign"`
+	Title    string `json:"title"`
+	Message  string `json:"message"`
+	URL      string `json:"url"`
+}
+
+type announcement struct {
+	Pro    bool   `json:"pro"`
+	Free   bool   `json:"free"`
+	Expiry string `json:"expiry"`
+	Announcement
+}
+
+func a(title, msg string) {
+	logo := ui.AddToken("/img/lantern_logo.png")
+	note := &notify.Notification{
+		Title:    title,
+		Message:  msg,
+		ClickURL: ui.GetUIAddr(),
+		IconURL:  logo,
+	}
+	_ = note
+}
+
+func GetAnnouncement(hc *http.Client, lang string, isPro bool, isStaging bool) (*Announcement, error) {
+	u := loConfURL
+	if isStaging {
+		u = stagingLoConfURL
+	}
+	b, efetch := fetch(hc, u)
+	if efetch != nil {
+		return nil, errors.Wrap(efetch)
+	}
+	parsed, eparse := parse(b, lang)
+	if eparse != nil {
+		return nil, errors.Wrap(eparse)
+	}
+
+	if parsed.Expiry != "" {
+		expiry, eexpiry := time.Parse("2006-01-02", parsed.Expiry)
+		if eexpiry != nil {
+			return nil, errors.Wrap(eexpiry)
+		}
+		y, m, d := time.Now().Date()
+		if !expiry.After(time.Date(y, m, d, 0, 0, 0, 0, time.UTC)) {
+			return nil, eNoAvailable
+		}
+	}
+
+	if isPro && parsed.Pro || !isPro && parsed.Free {
+		return &parsed.Announcement, nil
+	}
+	return nil, eNoAvailable
+}
+
+func fetch(hc *http.Client, u string) ([]byte, error) {
+	req, ereq := http.NewRequest("GET", u, nil)
+	if ereq != nil {
+		return nil, ereq
+	}
+	resp, efetch := hc.Do(req)
+	if efetch != nil {
+		return nil, efetch
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New("Unexpected status %v", resp.StatusCode)
+	}
+	return ioutil.ReadAll(resp.Body)
+}
+
+func parse(buf []byte, lang string) (*announcement, error) {
+	obj := map[string]interface{}{}
+	if ejson := json.Unmarshal(buf, &obj); ejson != nil {
+		return nil, ejson
+	}
+	section, ok := obj["announcement"]
+	if section == nil {
+		return nil, errors.New("No announcement section")
+	}
+	announcement, ok := section.(map[string]interface{})
+	if !ok {
+		return nil, eIncorrectType
+	}
+	inLang, exist := announcement[lang]
+	if !exist {
+		defaultLoc := announcement["default"]
+		if defaultLang, ok := defaultLoc.(string); !ok {
+			return nil, errors.New("No announcement for %s", lang)
+		} else if inLang, exist = announcement[defaultLang]; !exist {
+			return nil, errors.New("No announcement for either %s or %s", lang, defaultLang)
+		}
+	}
+	if inLang, ok := inLang.(map[string]interface{}); !ok {
+		return nil, eIncorrectType
+	} else {
+		return mapToAnnouncement(inLang)
+	}
+}
+
+func mapToAnnouncement(m map[string]interface{}) (*announcement, error) {
+	var errorFields []string
+	retval := &announcement{
+		Pro:    exactBool(m, "pro", &errorFields),
+		Free:   exactBool(m, "free", &errorFields),
+		Expiry: exactString(m, "expiry", &errorFields),
+		Announcement: Announcement{
+			Campaign: exactString(m, "campaign", &errorFields),
+			Title:    exactString(m, "title", &errorFields),
+			Message:  exactString(m, "message", &errorFields),
+			URL:      exactString(m, "url", &errorFields),
+		},
+	}
+	if len(errorFields) > 0 {
+		return nil, errors.New("Invalid fields %v", errorFields)
+	}
+	return retval, nil
+}
+
+func exactBool(m map[string]interface{}, key string, errorFields *[]string) bool {
+	if v, ok := m[key]; ok {
+		if b, ok := v.(bool); ok {
+			return b
+		}
+	}
+	*errorFields = append(*errorFields, key)
+	return false
+}
+
+func exactString(m map[string]interface{}, key string, errorFields *[]string) string {
+	if v, ok := m[key]; ok {
+		if s, ok := v.(string); ok {
+			return s
+		}
+	}
+	*errorFields = append(*errorFields, key)
+	return ""
+}
---
+++ announcement/announcement_test.go
@@ -0,0 +1,167 @@
+package app
+
+import (
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+const normalBody string = `{
+	"announcement": {
+		"default": "en-US",
+		"en-US": {
+			"campaign": "20160801",
+			"expiry": "",
+			"pro": true,
+			"free": true,
+			"title": "Try out the new feature",
+			"message": "Believe or not, you'll definitely love it!",
+			"url": ""
+		}
+	},
+	"survey": {
+		"anything": "else"
+	}
+}`
+
+func TestParseLoConf(t *testing.T) {
+	testCases := []struct {
+		body string
+		lang string
+		err  string
+	}{
+		{`{}`, "en-US", "No announcement section"},
+		{`{ "announcement": "" }`, "zh-CN", "Incorrect type"},
+		{`{
+			"announcement": {
+				"en-US": {}
+			}
+		}`, "zh-CN", "No announcement for zh-CN"},
+		{`{
+			"announcement": {
+				"default": "en-US",
+				"fr": ""
+			}
+		}`, "zh-CN", "No announcement for either zh-CN or en-US"},
+		{`{
+			"announcement": {
+				"default": "en-US",
+				"en-US": ""
+			}
+		}`, "zh-CN", "Incorrect type"},
+		{`{
+			"announcement": {
+				"default": "en-US",
+				"zh-CN": ""
+			}
+		}`, "zh-CN", "Incorrect type"},
+		{normalBody, "en-US", ""},
+		{normalBody, "zh-CN", ""},
+	}
+	for _, c := range testCases {
+		if !doTestParse(t, c.body, c.lang, c.err) {
+			t.Logf("%+v", c)
+		}
+	}
+}
+
+func doTestParse(t *testing.T, body, lang string, expected string) bool {
+	_, err := parse([]byte(body), lang)
+	if expected != "" {
+		if assert.Error(t, err, "should have error") {
+			return assert.Contains(t, err.Error(), expected)
+		}
+		return false
+	}
+	return assert.NoError(t, err, "should parse without error")
+}
+
+func TestParseAnnouncement(t *testing.T) {
+	for _, lang := range []string{"en-US", "zh-CN"} {
+		parsed, err := parse([]byte(normalBody), lang)
+		if !assert.NoError(t, err) {
+			continue
+		}
+		assert.Equal(t, parsed.Campaign, "20160801")
+		assert.Equal(t, parsed.Pro, true)
+		assert.Equal(t, parsed.Free, true)
+		assert.Equal(t, parsed.Title, "Try out the new feature")
+		assert.Equal(t, parsed.Message, "Believe or not, you'll definitely love it!")
+		assert.Equal(t, parsed.URL, "")
+	}
+}
+
+type mockTransport struct {
+	body string
+}
+
+func (m mockTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	return &http.Response{
+		StatusCode:    200,
+		Header:        http.Header{},
+		ContentLength: int64(len(m.body)),
+		Body:          ioutil.NopCloser(strings.NewReader(m.body)),
+	}, nil
+}
+
+func hcWithBody(body string) *http.Client {
+	return &http.Client{
+		Transport: mockTransport{
+			body: body,
+		},
+	}
+}
+
+func TestUserType(t *testing.T) {
+	_, err := GetAnnouncement(hcWithBody(normalBody), "en-US", false, false)
+	assert.NoError(t, err,
+		"getting announcement for free should have no error")
+	_, err = GetAnnouncement(hcWithBody(normalBody), "en-US", true, false)
+	assert.NoError(t, err,
+		"getting announcement for pro should have no error")
+
+	notForPro := strings.Replace(normalBody, `"pro": true`, `"pro": false`, 1)
+	_, err = GetAnnouncement(hcWithBody(notForPro), "en-US", false, false)
+	assert.NoError(t, err,
+		"getting announcement for free should have no error")
+	_, err = GetAnnouncement(hcWithBody(notForPro), "en-US", true, false)
+	if assert.Error(t, err,
+		"getting announcement for pro should have error") {
+		assert.Contains(t, err.Error(), "No announcement available")
+	}
+
+	notForFree := strings.Replace(normalBody, `"free": true`, `"free": false`, 1)
+	_, err = GetAnnouncement(hcWithBody(notForFree), "en-US", false, false)
+	if assert.Error(t, err,
+		"getting announcement for free should have error") {
+		assert.Contains(t, err.Error(), "No announcement available")
+	}
+	_, err = GetAnnouncement(hcWithBody(notForFree), "en-US", true, false)
+	assert.NoError(t, err,
+		"getting announcement for pro should have no error")
+}
+
+func TestExpiry(t *testing.T) {
+	today := `"expiry": "` + time.Now().Format("2006-01-02") + `"`
+	expired := strings.Replace(normalBody, `"expiry": ""`, today, 1)
+	_, err := GetAnnouncement(hcWithBody(expired), "en-US", false, false)
+	if assert.Error(t, err,
+		"expired announcement should have error") {
+		assert.Contains(t, err.Error(), "No announcement available")
+	}
+
+	nextDay := `"expiry": "` + time.Now().Add(24*time.Hour).Format("2006-01-02") + `"`
+	valid := strings.Replace(normalBody, `"expiry": ""`, nextDay, 1)
+	_, err = GetAnnouncement(hcWithBody(valid), "en-US", false, false)
+	assert.NoError(t, err,
+		"not expired announcement should have no error")
+
+	invalid := strings.Replace(normalBody, `"expiry": ""`, `"expiry": "9999-12-32"`, 1)
+	_, err = GetAnnouncement(hcWithBody(invalid), "en-US", false, false)
+	assert.Error(t, err,
+		"announcement with invalid expiry format should have error")
+}
