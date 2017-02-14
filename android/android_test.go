a09df13399742c379a377bfc435d365b83a08f8b
Myles Horton <myles@getlantern.org>
2017-02-13 17:21:02 -0800

    added missing method

--- android/android_test.go
+++ android/android_test.go
@@ -27,8 +27,10 @@ func (c testUserConfig) ConfigUpdate(bool)        {}
 func (c testUserConfig) ShowSurvey(survey string) {}
 func (c testUserConfig) GetUserID() int64         { return 0 }
 func (c testUserConfig) GetToken() string         { return "" }
+func (c testUserConfig) SetStaging(bool)          {}
 
 func TestProxying(t *testing.T) {
+
 	tmpDir, err := ioutil.TempDir("", "testconfig")
 	if assert.NoError(t, err, "Unable to create temp configDir") {
 		defer os.RemoveAll(tmpDir)
