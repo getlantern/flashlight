c281ba451c60bb494f87fe06f69a0d87cb94f856
Myles Horton <myles@getlantern.org>
2017-02-23 11:53:19 -0800

    added error handling

--- hitProxy.py
+++ hitProxy.py
@@ -5,14 +5,23 @@ import sys
 import json
 import yaml
 import tempfile
-from subprocess import call
+import subprocess
 
 if len(sys.argv) != 2:
     print "Usage: %s <ip>" % sys.argv[0]
     sys.exit(1)
 
 ip = sys.argv[1]
-call(["scp", "lantern@" + ip + ":access_data.json", "."])
+
+try:
+    subprocess.check_call(["scp", "lantern@" + ip + ":access_data.json", "."])
+except subprocess.CalledProcessError:
+    print "Error copying access data from "+ip
+    sys.exit(1)
+except OSError:
+    print "Error running scp"
+    sys.exit(1)
+
 loaded = json.loads(open("access_data.json").read())
 
 servers = {}
@@ -27,4 +36,4 @@ f = open(p, "w")
 f.write(yaml.safe_dump(servers, encoding='utf-8', allow_unicode=True, default_flow_style=False))
 f.close()
 
-call(["./lantern", "-stickyconfig", "-readableconfig", "-configdir="+tmpdir])
+subprocess.call(["./lantern", "-stickyconfig", "-readableconfig", "-configdir="+tmpdir])
