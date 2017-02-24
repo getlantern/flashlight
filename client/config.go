2a0bca8c9be8f590605c4b8da851a7a19cb6d7ef
joesis <joesis@getlantern.org>
2017-02-24 10:30:02 +0800

    remove unused client.ChainedServers

--- client/config.go
+++ client/config.go
@@ -2,8 +2,6 @@ package client
 
 import (
 	"github.com/getlantern/fronted"
-
-	"github.com/getlantern/flashlight/chained"
 )
 
 // ClientConfig captures configuration information for a Client
@@ -11,14 +9,12 @@ type ClientConfig struct {
 	ShowAds bool // whether or not to show ads for a given client
 
 	DumpHeaders    bool // whether or not to dump headers of requests and responses
-	ChainedServers map[string]*chained.ChainedServerInfo
 	MasqueradeSets map[string][]*fronted.Masquerade
 }
 
 // NewConfig creates a new client config with default values.
 func NewConfig() *ClientConfig {
 	return &ClientConfig{
-		ChainedServers: make(map[string]*chained.ChainedServerInfo),
 		MasqueradeSets: make(map[string][]*fronted.Masquerade),
 	}
 }
