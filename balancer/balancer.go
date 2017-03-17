b46d19aa79c27f1fad71bd21592c588c1dd760e8
Ox Cart <ox.to.a.cart@gmail.com>
2017-03-17 15:55:16 -0500

    Reduced frequency of balancer stats readout

--- balancer/balancer.go
+++ balancer/balancer.go
@@ -259,7 +259,7 @@ func (b *Balancer) Close() {
 // printStats periodically prints out stats for all dialers
 func (b *Balancer) printStats() {
 	// TODO: print these less frequently
-	t := time.NewTicker(5 * time.Second)
+	t := time.NewTicker(30 * time.Second)
 	for {
 		select {
 		case <-b.stopStatsCh:
