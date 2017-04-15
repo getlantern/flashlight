a70fd3a3c83f3127b5c6af7dc405e15e537fd8c8
atavism <todd@lana.io>
2017-04-15 12:56:21 -0400

    add http util package

---
+++ util/http.go
@@ -0,0 +1,26 @@
+package util
+
+import (
+	"net/http"
+	"net/http/httputil"
+)
+
+func DumpRequest(resp *http.Response) {
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	log.Debugf("%q", dump)
+}
+
+func DumpResponse(resp *http.Response, body bool) {
+	dump, err := httputil.DumpResponse(resp, true)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+
+	log.Debugf("%q", dump)
+}
