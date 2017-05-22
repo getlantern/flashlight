00f92519000db4447dc84c2fe00c958941be137e
joesis <joesis@getlantern.org>
2017-05-22 14:18:07 +0800

    fix analytics test

--- analytics/analytics_test.go
+++ analytics/analytics_test.go
@@ -35,12 +35,14 @@ func TestAnalytics(t *testing.T) {
 	// Now actually hit the GA debug server to validate the hit.
 	url := "https://www.google-analytics.com/debug/collect?" + argString
 	resp, err := http.Get(url)
-	assert.Nil(t, err, "Should be nil")
+	if !assert.NoError(t, err, "Should have no error accessing GA") {
+		return
+	}
 
 	defer resp.Body.Close()
-
 	body, err := ioutil.ReadAll(resp.Body)
-	assert.Nil(t, err, "Should be nil")
-
+	if !assert.NoError(t, err, "Should have no error read body") {
+		return
+	}
 	assert.True(t, strings.Contains(string(body), "\"valid\": true"), "Should be a valid hit")
 }
