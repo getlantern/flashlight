925632b65bd98098d331cf54638d9a14f8388d63
Ox Cart <ox.to.a.cart@gmail.com>
2017-05-30 11:24:21 -0500

    Added SendEmail function to android package

---
+++ android/email.go
@@ -0,0 +1,15 @@
+package android
+
+import (
+	"github.com/getlantern/flashlight/email"
+)
+
+// EmailMessage exposes the type email.Message as part of this package.
+type EmailMessage struct {
+	email.Message
+}
+
+// SendEmail sends the given EmailMessage using the email package.
+func SendEmail(msg EmailMessage) error {
+	return email.Send(&msg.Message)
+}
