19f84af10343e61cc621e192e378f13c255614b6
atavism <todd@lana.io>
2017-10-02 13:59:50 -0700

    update comment

--- app/app.go
+++ app/app.go
@@ -154,7 +154,7 @@ func (app *App) Run() {
 				}
 				return ui.AddToken("/") + "#/plans"
 			},
-			func() bool { return false },             // check if app installed by play store doesn't apply to desktop
+			func() bool { return true },              // always allow ad blocking on desktop
 			func(addr string) string { return addr }, // no dnsgrab reverse lookups on desktop
 		)
 		if err != nil {
