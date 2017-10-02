52c7e37c8a008acd1966e95f08e2bd25a9e7073c
atavism <todd@lana.io>
2017-10-02 13:38:24 -0700

    fix test

--- android/android.go
+++ android/android.go
@@ -205,7 +205,7 @@ func run(configDir, locale string,
 		session.IsProUser,
 		func() string { return "" }, // only used for desktop
 		func() string { return "" }, // only used for desktop
-		func() bool { return session.IsPlayVersion() },
+		session.IsPlayVersion,
 		func(addr string) string {
 			host, port, splitErr := net.SplitHostPort(addr)
 			if splitErr != nil {
