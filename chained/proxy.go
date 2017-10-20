e3e65235d6168e49deb05b74e26c7bc6ec6f231e
Ox Cart <ox.to.a.cart@gmail.com>
2017-10-20 07:12:35 -0500

    Using kcpwrapper to achieve tighter integration with KCP

--- chained/proxy.go
+++ chained/proxy.go
@@ -1,6 +1,7 @@
 package chained
 
 import (
+	"context"
 	"crypto/rsa"
 	"crypto/tls"
 	"fmt"
@@ -17,13 +18,12 @@ import (
 
 	"github.com/getlantern/ema"
 	"github.com/getlantern/errors"
-	"github.com/getlantern/eventual"
 	"github.com/getlantern/flashlight/balancer"
 	"github.com/getlantern/flashlight/buffers"
 	"github.com/getlantern/flashlight/common"
 	"github.com/getlantern/flashlight/ops"
 	"github.com/getlantern/idletiming"
-	"github.com/getlantern/kcptun/client/lib"
+	"github.com/getlantern/kcpwrapper"
 	"github.com/getlantern/keyman"
 	"github.com/getlantern/lampshade"
 	"github.com/getlantern/mtime"
@@ -37,7 +37,7 @@ const (
 )
 
 var (
-	chainedDialTimeout          = 10 * time.Second
+	chainedDialTimeout          = 1000 * time.Second
 	theForceAddr, theForceToken string
 )
 
@@ -267,6 +267,7 @@ func newLampshadeProxy(name string, s *ChainedServerInfo, deviceID string, proTo
 type proxy struct {
 	// Store int64's up front to ensure alignment of 64 bit words
 	// See https://golang.org/pkg/sync/atomic/#pkg-note-BUG
+	dialCore          func(timeout time.Duration) (net.Conn, time.Duration, error)
 	attempts          int64
 	successes         int64
 	consecSuccesses   int64
@@ -311,6 +312,18 @@ func newProxy(name, protocol, network, addr string, s *ChainedServerInfo, device
 		consecSuccesses:  1, // be optimistic
 	}
 
+	p.dialCore = func(timeout time.Duration) (net.Conn, time.Duration, error) {
+		elapsed := mtime.Stopwatch()
+		conn, err := netx.DialTimeout("tcp", p.addr, timeout)
+		delta := elapsed()
+		if err == nil {
+			p.mx.Lock()
+			p.emaLatency.UpdateDuration(delta)
+			p.mx.Unlock()
+		}
+		return conn, delta, err
+	}
+
 	if s.KCPSettings != nil && len(s.KCPSettings) > 0 {
 		err := enableKCP(p, s)
 		if err != nil {
@@ -323,49 +336,27 @@ func newProxy(name, protocol, network, addr string, s *ChainedServerInfo, device
 }
 
 func enableKCP(p *proxy, s *ChainedServerInfo) error {
-	var conf lib.Config
-	err := mapstructure.Decode(s.KCPSettings, &conf)
+	var cfg KCPConfig
+	err := mapstructure.Decode(s.KCPSettings, &cfg)
 	if err != nil {
 		return log.Errorf("Could not decode kcp transport settings?: %v", err)
 	}
-	conf.LocalAddr = "127.0.0.1:0"
 
-	startResult := eventual.NewValue()
-	go func() {
-		err := lib.Run(&conf, "embedded", func(addr net.Addr) {
-			startResult.Set(addr.String())
-		})
-		if err != nil {
-			log.Errorf("Error running kcp client: %v", err)
-			startResult.Set(err)
-		}
-	}()
+	// Fix address (comes across as kcp-placeholder)
+	p.addr = cfg.RemoteAddr
 
-	startTimeout := 5 * time.Second
-	result, ok := startResult.Get(startTimeout)
-	if !ok {
-		return log.Errorf("kcp client failed to start in %v", startTimeout)
+	dialKCP := kcpwrapper.Dialer(&cfg.DialerConfig)
+	p.dialCore = func(timeout time.Duration) (net.Conn, time.Duration, error) {
+		elapsed := mtime.Stopwatch()
+		ctx, cancel := context.WithTimeout(context.Background(), timeout)
+		defer cancel()
+		log.Debug("Dialing KCP")
+		conn, err := dialKCP(ctx, "tcp", p.addr)
+		log.Debugf("Dialed KCP: %v : %v", conn, err)
+		return conn, elapsed(), err
 	}
-	switch t := result.(type) {
-	case string:
-		log.Debugf("kcp client running at %v", t)
-		// Right now, we don't have a good way estimating performance of KCP-based
-		// proxies, so we just mark them as "preferred" to force them to get used by
-		// default.
-		p.preferred = true
-		p.addr = t
-		p.protocol = p.protocol + "-kcp"
-		if conf.Key != "" && conf.Crypt != "none" && conf.Crypt != "xor" {
-			// We're using kcp's built-in encryption, so we can consider the dialer
-			// trusted
-			p.trusted = true
-		}
-		return nil
-	case error:
-		return t
-	default:
-		return log.Errorf("Unkown start result type: %v", t)
-	}
+
+	return nil
 }
 
 func (p *proxy) Protocol() string {
@@ -512,20 +503,16 @@ func (p *proxy) tcpDial(op *ops.Op) func(timeout time.Duration) (net.Conn, error
 		if estBandwidth > 0 {
 			op.Set("est_mbps", estBandwidth)
 		}
-		conn, delta, err := p.dialTCP(timeout)
+		conn, delta, err := p.dialCore(timeout)
+		log.Debugf("Got conn: %v", conn)
 		op.TCPDialTime(delta, err)
 		return overheadWrapper(false)(conn, err)
 	}
 }
 
-func (p *proxy) dialTCP(timeout time.Duration) (net.Conn, time.Duration, error) {
-	elapsed := mtime.Stopwatch()
-	conn, err := netx.DialTimeout("tcp", p.addr, timeout)
-	delta := elapsed()
-	if err == nil {
-		p.mx.Lock()
-		p.emaLatency.UpdateDuration(delta)
-		p.mx.Unlock()
-	}
-	return conn, delta, err
+// KCPConfig adapts kcpwrapper.DialerConfig to the currently deployed
+// configurations in order to provide backward-compatibility.
+type KCPConfig struct {
+	kcpwrapper.DialerConfig `mapstructure:",squash"`
+	RemoteAddr              string `json:"remoteaddr"`
 }
