a470d57ec301f80e5436216e89c14cd375382cde
Ox Cart <ox.to.a.cart@gmail.com>
2017-11-16 13:50:25 -0600

    Removed sleep from integration test

--- app/integration_test.go
+++ app/integration_test.go
@@ -168,7 +168,6 @@ func TestProxying(t *testing.T) {
 
 	select {
 	case <-onGeo:
-		time.Sleep(30 * time.Second)
 		opsMx.RLock()
 		for _, op := range flashlight.FullyReportedOps {
 			if op == "report_issue" || op == "sysproxy_off" || op == "sysproxy_off_force" || op == "sysproxy_clear" || op == "probe" {
