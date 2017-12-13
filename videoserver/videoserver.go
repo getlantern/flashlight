5f600f27c47161e94be3d175faec377f52cd0e6c
joesis <joesis@getlantern.org>
2017-12-12 16:16:11 -0800

    server video list from flashlight

--- videoserver/videoserver.go
+++ videoserver/videoserver.go
@@ -7,15 +7,48 @@ import (
 	"os/exec"
 	"path"
 	"strings"
+	"time"
 
+	"github.com/getlantern/eventual"
 	"github.com/getlantern/golog"
+
+	"github.com/getlantern/flashlight/loconf"
 )
 
 var (
-	log = golog.LoggerFor("flashlight.videoserver")
+	log       = golog.LoggerFor("flashlight.videoserver")
+	videoList = eventual.NewValue()
 )
 
+func FetchLoop() func() {
+	tk := time.NewTicker(1 * time.Hour)
+	ch := make(chan bool)
+	stop := func() { ch <- true }
+	go func() {
+		for {
+			b, err := loconf.GetPopVideos(http.DefaultClient)
+			if err != nil {
+				log.Error(err)
+			} else {
+				videoList.Set(b)
+			}
+
+			select {
+			case <-tk.C:
+			case <-ch:
+				tk.Stop()
+				return
+			}
+		}
+	}()
+	return stop
+}
+
 func ServeVideo(resp http.ResponseWriter, req *http.Request) {
+	if req.URL.RawQuery == "" {
+		serveList(resp, req)
+		return
+	}
 	videoHash := strings.Split(path.Base(req.URL.Query().Get("v")), ".")[0]
 	log.Debugf("Serving video: %v", videoHash)
 	cmd := exec.Command("ipfs", "cat", videoHash)
@@ -27,5 +60,14 @@ func ServeVideo(resp http.ResponseWriter, req *http.Request) {
 	err := cmd.Run()
 	if err != nil {
 		log.Errorf("Error reading %v from ipfs: %v", videoHash, err)
+	}
+}
+
+func serveList(resp http.ResponseWriter, req *http.Request) {
+	b, valid := videoList.Get(0)
+	if valid {
+		resp.Write(b.([]byte))
+	} else {
+		resp.WriteHeader(http.StatusServiceUnavailable)
 	}
 }
