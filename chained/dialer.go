e230ff1481c8b265df6b5737941a3f3488b00a63
Ox Cart <ox.to.a.cart@gmail.com>
2018-05-03 12:58:35 -0500

    Took out noisy debug log

--- chained/dialer.go
+++ chained/dialer.go
@@ -115,7 +115,6 @@ func (p *proxy) ConsecFailures() int64 {
 }
 
 func (p *proxy) Succeeding() bool {
-	log.Debugf("%d / %d : %d", p.ConsecSuccesses(), p.ConsecFailures(), p.consecRWSuccesses.Get())
 	// To avoid turbulence when network glitches, treat proxies with a small
 	// amount failures as succeeding.
 	// TODO: OTOH, when the proxy just recovered from failing, should wait for
