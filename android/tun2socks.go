dc60b107aa815d157b2c175b6d3819b92db50949
Ox Cart <ox.to.a.cart@gmail.com>
2018-12-04 10:06:09 -0800

    Added pure Go tun2socks

---
+++ android/tun2socks.go
@@ -0,0 +1,25 @@
+package android
+
+import (
+	"github.com/getlantern/errors"
+	"github.com/getlantern/gotun2socks"
+	"github.com/getlantern/gotun2socks/tun"
+)
+
+func Tun2Socks(fd int, socksAddr string, dnsServer string) error {
+	dev, err := tun.WrapTunDevice(fd)
+	if err != nil {
+		return errors.New("Unable to wrap TUN device: %v", err)
+	}
+
+	tun := gotun2socks.New(dev,
+		socksAddr,
+		[]string{dnsServer},
+		true,  // public traffic only
+		false, // don't cache DNS
+	)
+	go tun.Run()
+
+	log.Debug("Listening for TUN traffic")
+	return nil
+}
