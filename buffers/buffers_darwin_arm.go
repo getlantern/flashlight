3b854f35e8dcb1012ec733e076e23683b7bd3775
Ox Cart <ox.to.a.cart@gmail.com>
2019-02-22 14:09:31 -0600

    Reduced number of buffers used on iOS

---
+++ buffers/buffers_darwin_arm.go
@@ -0,0 +1,8 @@
+// +build !darwin
+// +build !arm
+
+package buffers
+
+const (
+	maxBufferBytes = 128 * 1024 // use a small amount of buffers on iOS to avoid running afoul of memory limit
+)
---
+++ buffers/buffers_other.go
@@ -0,0 +1,8 @@
+// +build !darwin
+// +build !arm
+
+package buffers
+
+const (
+	maxBufferBytes = 30 * 1024 * 1024
+)
