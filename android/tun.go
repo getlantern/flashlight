9ab91518fa248edf7d7076723d95784f8409cf21
Ox Cart <ox.to.a.cart@gmail.com>
2019-03-07 11:50:36 -0600

    Switched to using ipproxy

--- android/tun.go
+++ android/tun.go
@@ -2,22 +2,25 @@ package android
 
 import (
 	"context"
+	"io"
 	"net"
 	"sync"
 
 	"github.com/getlantern/errors"
 	"github.com/getlantern/gotun"
+	"github.com/getlantern/ipproxy"
 	"github.com/getlantern/netx"
 	"golang.org/x/net/proxy"
 )
 
 var (
 	currentDeviceMx sync.Mutex
-	currentDevice   tun.TUNDevice
+	currentDevice   io.ReadWriteCloser
+	currentIPP      ipproxy.Proxy
 )
 
-// Tun2Socks wraps the TUN device identified by fd with a gotun server that does
-// the following:
+// Tun2Socks wraps the TUN device identified by fd with an ipproxy server that
+// does the following:
 //
 // 1. dns packets bound for dnsAddr are rerouted to dnsgrab at dnsGrabAddr
 // 2. All other udp packets are routed directly to their destination
@@ -29,34 +32,43 @@ func Tun2Socks(fd int, tunAddr, gwAddr, socksAddr, dnsAddr, dnsGrabAddr string, 
 	if err != nil {
 		return errors.New("Unable to wrap tun device: %v", err)
 	}
+	defer dev.Close()
+
 	socksDialer, err := proxy.SOCKS5("tcp", socksAddr, nil, nil)
 	if err != nil {
 		return errors.New("Unable to get SOCKS5 dialer: %v", err)
 	}
+
+	ipp, err := ipproxy.New(dev, &ipproxy.Opts{
+		MTU: mtu,
+		DialTCP: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return socksDialer.Dial(network, addr)
+		},
+		DialUDP: func(ctx context.Context, network, addr string) (*net.UDPConn, error) {
+			if addr == dnsAddr {
+				// reroute DNS requests to dnsgrab
+				addr = dnsGrabAddr
+			}
+			raddr, err := netx.ResolveUDPAddr(network, addr)
+			if err != nil {
+				return nil, err
+			}
+			return netx.DialUDP(network, nil, raddr)
+		},
+	})
+	if err != nil {
+		return errors.New("Unable to create ipproxy: %v", err)
+	}
+
 	currentDeviceMx.Lock()
 	currentDevice = dev
+	currentIPP = ipp
 	currentDeviceMx.Unlock()
 
 	go func() {
-		err := tun.NewBridge(dev, &tun.BridgeOpts{
-			MTU: mtu,
-			DialTCP: func(ctx context.Context, network, addr string) (net.Conn, error) {
-				return socksDialer.Dial(network, addr)
-			},
-			DialUDP: func(ctx context.Context, network, addr string) (*net.UDPConn, error) {
-				if addr == dnsAddr {
-					// reroute DNS requests to dnsgrab
-					addr = dnsGrabAddr
-				}
-				raddr, err := netx.ResolveUDPAddr(network, addr)
-				if err != nil {
-					return nil, err
-				}
-				return netx.DialUDP(network, nil, raddr)
-			},
-		}).Serve()
-		if err != nil {
-			log.Errorf("Error on serving from TUN device: %v", err)
+		serveErr := ipp.Serve()
+		if serveErr != nil {
+			log.Errorf("Error on serving from TUN device: %v", serveErr)
 		}
 	}()
 
@@ -67,11 +79,21 @@ func Tun2Socks(fd int, tunAddr, gwAddr, socksAddr, dnsAddr, dnsGrabAddr string, 
 func StopTun2Socks() {
 	currentDeviceMx.Lock()
 	dev := currentDevice
+	ipp := currentIPP
 	currentDevice = nil
 	currentDeviceMx.Unlock()
 	if dev != nil {
-		log.Debug("Stopping TUN device")
-		dev.Stop()
-		log.Debug("Stopped TUN device")
+		log.Debug("Closing TUN device")
+		if err := dev.Close(); err != nil {
+			log.Errorf("Error closing TUN device: %v", err)
+		}
+		log.Debug("Closed TUN device")
+	}
+	if ipp != nil {
+		log.Debug("Closing ipproxy")
+		if err := currentIPP.Close(); err != nil {
+			log.Errorf("Error closing ipproxy: %v", err)
+		}
+		log.Debug("Closed ipproxy")
 	}
 }
