c774054441b77bee8d63fe38031323824bf60295
thistle <bcmertz@email.wm.edu>
2019-06-11 12:44:11 -0400

    print for linux too

--- chained/utp_android_linux.go
+++ chained/utp_android_linux.go
@@ -3,5 +3,5 @@ package chained
 import "errors"
 
 func enableUTP(p *proxy, s *ChainedServerInfo) error {
-	return errors.New("UTP is not supported on Android")
+	return errors.New("UTP is not supported on Android or Linux")
 }
