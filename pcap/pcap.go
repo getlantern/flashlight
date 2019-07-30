0e5f565e2ef8fda8ba3a845bd4dd05e1f81d7761
Joesis <joesis@getlantern.org>
2019-07-30 00:44:27 -0700

    capture all proxies in one file to check the time sequence more easily

--- pcap/pcap.go
+++ pcap/pcap.go
@@ -16,26 +16,28 @@ import (
 var (
 	log               = golog.LoggerFor("flashlight.pcap")
 	logdir            = appdir.Logs("Lantern")
-	params_template   = "-ni en0 -p -w %s/%s.pcapng -C 1 -W 10 -s 200 host %s and not port ssh"
+	params_template   = "-ni en0 -p -w %s/proxies.pcapng -C 1 -W 10 -s 200 (host %s) and not port ssh"
 	activeProcesses   []*exec.Cmd
 	muActiveProcesses sync.Mutex
 )
 
 func Configure(proxies map[string]*chained.ChainedServerInfo) {
 	var processes []*exec.Cmd
-	for name, config := range proxies {
+	hosts := make([]string, 0, len(proxies))
+	for _, config := range proxies {
 		host, _, err := net.SplitHostPort(config.Addr)
 		if err != nil {
 			host = config.Addr
 		}
-		params := fmt.Sprintf(params_template, logdir, name, host)
-		cmd := exec.Command("tcpdump", strings.Split(params, " ")...)
-		log.Debugf("Starting '%v'", cmd.Args)
-		if err := cmd.Start(); err != nil {
-			log.Errorf("failed to start '%v': %v", cmd.Args, err)
-		} else {
-			processes = append(processes, cmd)
-		}
+		hosts = append(hosts, host)
+	}
+	params := fmt.Sprintf(params_template, logdir, strings.Join(hosts, " or "))
+	cmd := exec.Command("tcpdump", strings.Split(params, " ")...)
+	log.Debugf("Starting '%v'", cmd.Args)
+	if err := cmd.Start(); err != nil {
+		log.Errorf("failed to start '%v': %v", cmd.Args, err)
+	} else {
+		processes = append(processes, cmd)
 	}
 	muActiveProcesses.Lock()
 	existing := activeProcesses
