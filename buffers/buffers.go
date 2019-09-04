0069a522983dcc6d9e16d16ef1f8910884d1dd23
Myles Horton <myles@getlantern.org>
2019-09-04 15:47:48 -0700

    use sync pool since it appears slightly faster

--- buffers/buffers.go
+++ buffers/buffers.go
@@ -5,7 +5,7 @@ import (
 )
 
 // Pool is a pool of buffers
-var Pool = lampshade.NewBufferPool(maxBufferBytes)
+var Pool = lampshade.NewBufferSyncPool()
 
 // MaxBufferBytes exposes the configured maxBufferBytes
 func MaxBufferBytes() int {
