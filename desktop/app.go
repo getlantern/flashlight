198032e7522ea64f8c017e7dc14b35e9cbe5be93
Matt Joiner <anacrolix@gmail.com>
2019-12-17 17:57:58 +1100

    Extract NewReplicaHttpServer in desktop app init

--- desktop/app.go
+++ desktop/app.go
@@ -15,6 +15,7 @@ import (
 
 	"github.com/getlantern/appdir"
 	"github.com/getlantern/eventual"
+	"golang.org/x/xerrors"
 
 	"github.com/anacrolix/confluence/confluence"
 	analog "github.com/anacrolix/log" // heuheuehue
@@ -285,59 +286,12 @@ func (app *App) beforeStart(listenAddr string) func() bool {
 			}
 		}
 
-		userCacheDir, err := os.UserCacheDir()
+		replicaHandler, exitFunc, err := NewReplicaHttpServer()
 		if err != nil {
-			panic(err)
-		}
-		replicaLogger := analog.Default
-		const replicaDirElem = "replica"
-		replicaConfigDir := appdir.General(replicaDirElem)
-		replicaUploadsDir := filepath.Join(replicaConfigDir, "uploads")
-		replicaDataDir := filepath.Join(userCacheDir, replicaDirElem, "data")
-		cfg := torrent.NewDefaultClientConfig()
-		cfg.DataDir = replicaDataDir
-		cfg.Seed = true
-		//cfg.Debug = true
-		cfg.Logger = replicaLogger.WithFilter(func(m analog.Msg) bool {
-			return !m.HasValue("upnp-discover")
-		})
-		// DHT disabled pending fixes to transaction query concurrency.	 Also using S3 as a
-		// fallback means we have a great tracker to rely on.
-		//cfg.NoDHT = true
-		// Household users may be behind a NAT/bad router, or on a limited device like a mobile. We
-		// don't want to overload their networks, so ensure the default connection tracking
-		// behaviour.
-		//cfg.ConnTracker = conntrack.NewInstance()
-		// Helps debug connection tracking, for best configuring DHT and other limits.
-		http.HandleFunc("/debug/conntrack", func(w http.ResponseWriter, r *http.Request) {
-			w.Header().Set("Content-Type", "text/plain")
-			cfg.ConnTracker.PrintStatus(w)
-		})
-		torrentClient, err := torrent.NewClient(cfg)
-		if err != nil {
-			log.Errorf("error starting torrent client: %v", err)
+			log.Errorf("error creating replica http server: %v", err)
 			app.Exit(err)
 		}
-		app.AddExitFunc("close torrent client", torrentClient.Close)
-
-		app.AddExitFunc("torrent", func() {
-			torrentClient.Close()
-		})
-
-		if err := replica.IterUploads(replicaUploadsDir, func(mi *metainfo.MetaInfo, err error) {
-			if err != nil {
-				replicaLogger.Printf("error while iterating uploads: %v", err)
-				return
-			}
-			t, err := torrentClient.AddTorrent(mi)
-			if err != nil {
-				replicaLogger.WithValues(analog.Error).Printf("error adding existing upload to torrent client: %v", err)
-			} else {
-				replicaLogger.Printf("added previous upload %q to torrent client", t.Name())
-			}
-		}); err != nil {
-			panic(err)
-		}
+		app.AddExitFunc("cleanup replica http server", exitFunc)
 
 		log.Debugf("Starting client UI at %v", uiaddr)
 
@@ -355,19 +309,7 @@ func (app *App) beforeStart(listenAddr string) func() bool {
 				Pattern: "/replica/",
 				Handler: http.StripPrefix(
 					"/replica",
-					&ui.ReplicaHttpServer{
-						Confluence: confluence.Handler{
-							TC: torrentClient,
-							MetainfoCacheDir: func() *string {
-								s := filepath.Join(userCacheDir, replicaDirElem, "metainfos")
-								return &s
-							}(),
-						},
-						TorrentClient: torrentClient,
-						DataDir:       replicaDataDir,
-						Logger:        replicaLogger,
-						UploadsDir:    replicaUploadsDir,
-					}),
+					replicaHandler),
 			},
 		); err != nil {
 			app.Exit(fmt.Errorf("Unable to start UI: %s", err))
@@ -421,6 +363,71 @@ func (app *App) beforeStart(listenAddr string) func() bool {
 		})
 		return true
 	}
+}
+
+func NewReplicaHttpServer() (_ *ui.ReplicaHttpServer, exitFunc func(), err error) {
+	userCacheDir, err := os.UserCacheDir()
+	if err != nil {
+		panic(err)
+	}
+	replicaLogger := analog.Default
+	const replicaDirElem = "replica"
+	replicaConfigDir := appdir.General(replicaDirElem)
+	uploadsDir := filepath.Join(replicaConfigDir, "uploads")
+	replicaDataDir := filepath.Join(userCacheDir, replicaDirElem, "data")
+	cfg := torrent.NewDefaultClientConfig()
+	cfg.DataDir = replicaDataDir
+	cfg.Seed = true
+	//cfg.Debug = true
+	cfg.Logger = replicaLogger.WithFilter(func(m analog.Msg) bool {
+		return !m.HasValue("upnp-discover")
+	})
+	// DHT disabled pending fixes to transaction query concurrency.	 Also using S3 as a
+	// fallback means we have a great tracker to rely on.
+	//cfg.NoDHT = true
+	// Household users may be behind a NAT/bad router, or on a limited device like a mobile. We
+	// don't want to overload their networks, so ensure the default connection tracking
+	// behaviour.
+	//cfg.ConnTracker = conntrack.NewInstance()
+	// Helps debug connection tracking, for best configuring DHT and other limits.
+	http.HandleFunc("/debug/conntrack", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		cfg.ConnTracker.PrintStatus(w)
+	})
+	torrentClient, err := torrent.NewClient(cfg)
+	if err != nil {
+		err = xerrors.Errorf("starting torrent client: %w", err)
+		return
+	}
+
+	if err := replica.IterUploads(uploadsDir, func(mi *metainfo.MetaInfo, err error) {
+		if err != nil {
+			replicaLogger.Printf("error while iterating uploads: %v", err)
+			return
+		}
+		t, err := torrentClient.AddTorrent(mi)
+		if err != nil {
+			replicaLogger.WithValues(analog.Error).Printf("error adding existing upload to torrent client: %v", err)
+		} else {
+			replicaLogger.Printf("added previous upload %q to torrent client", t.Name())
+		}
+	}); err != nil {
+		panic(err)
+	}
+
+	return &ui.ReplicaHttpServer{
+		Confluence: confluence.Handler{
+			TC: torrentClient,
+			MetainfoCacheDir: func() *string {
+				s := filepath.Join(userCacheDir, replicaDirElem, "metainfos")
+				return &s
+			}(),
+		},
+		TorrentClient: torrentClient,
+		DataDir:       replicaDataDir,
+		Logger:        replicaLogger,
+		UploadsDir:    uploadsDir,
+	}, torrentClient.Close, nil
 }
 
 // Connect turns on proxying
