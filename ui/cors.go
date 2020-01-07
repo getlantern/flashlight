5a358af5e3a3ec13feeb79f9300a7f2e52c0eb5a
atavism <todd@lana.io>
2020-01-07 14:25:46 -0800

    add accessAddr instead of listenAddr to cors origins

--- ui/cors.go
+++ ui/cors.go
@@ -22,7 +22,7 @@ var corsAllowedHeaders = []string{
 }
 
 func (s *Server) corsHandler(next http.Handler) http.Handler {
-	uiAddr := fmt.Sprintf("http://%s", s.listenAddr)
+	uiAddr := fmt.Sprintf("http://%s", s.accessAddr)
 	corsOrigins = append(corsOrigins, uiAddr)
 	cors := cors.New(cors.Options{
 		AllowedOrigins:   corsOrigins,
