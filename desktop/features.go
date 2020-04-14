5f98a35eb5d668debcb5b5f0c99bcd34c41720a1
Joesis <joesis@getlantern.org>
2020-04-14 00:17:03 -0700

    remove the extra goroutine in startFeaturesService

--- desktop/features.go
+++ desktop/features.go
@@ -8,25 +8,24 @@ import (
 // frontend.
 func startFeaturesService(channel ws.UIChannel, enabledFeatures func() map[string]bool,
 	chans ...<-chan bool) {
-	if service, err := channel.Register("features", func(write func(interface{})) {
+	service, err := channel.Register("features", func(write func(interface{})) {
 		log.Debugf("Sending features enabled to new client")
 		write(enabledFeatures())
-	}); err != nil {
+	})
+	if err != nil {
 		log.Errorf("Unable to serve enabled features to UI: %v", err)
-	} else {
-		go func() {
-			for _, ch := range chans {
-				go func(c <-chan bool) {
-					for range c {
-						select {
-						case service.Out <- enabledFeatures():
-							// ok
-						default:
-							// don't block if no-one is listening
-						}
-					}
-				}(ch)
+		return
+	}
+	for _, ch := range chans {
+		go func(c <-chan bool) {
+			for range c {
+				select {
+				case service.Out <- enabledFeatures():
+					// ok
+				default:
+					// don't block if no-one is listening
+				}
 			}
-		}()
+		}(ch)
 	}
 }
