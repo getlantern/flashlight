b35db8d7388a0803d26497ee0517836374fcfc50
Myles Horton <myles@getlantern.org>
2020-05-07 11:39:45 -0700

    fixed android startup

--- android/android.go
+++ android/android.go
@@ -330,13 +330,6 @@ func run(configDir, locale string,
 		// already have in desktop)
 		func() bool { return true }, // auto report
 		flags,
-		func() bool {
-			return true
-		}, // beforeStart()
-		func(c *client.Client) {
-			cl.Set(c)
-			afterStart(session)
-		},
 		func(cfg *config.Global, src config.Source) {
 			session.UpdateAdSettings(&adSettings{cfg.AdSettings})
 			email.SetDefaultRecipient(cfg.ReportIssueEmail)
