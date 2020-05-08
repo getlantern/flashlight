d8b7be5dde43a0c13e6dd45e9eba88ad38b32f22
Maxb <bittmanmax@gmail.com>
2020-05-07 17:24:33 -0700

    Use milliseconds and mbps for InstrumentedResponseWriter metrics

--- ops/instrumentedresponsewriter.go
+++ ops/instrumentedresponsewriter.go
@@ -28,10 +28,11 @@ func InitInstrumentedResponseWriter(w http.ResponseWriter, label string) *Instru
 }
 
 func (w *InstrumentedResponseWriter) Finish() {
-	totalTime := time.Now().Sub(w.start).Seconds()
-	w.Op.SetMetricPercentile(fmt.Sprintf("%s_response_time", w.label), totalTime)
-	rate := float64(w.written) / totalTime
-	w.Op.SetMetricPercentile(fmt.Sprintf("%s_response_rate", w.label), rate)
+	totalTime := time.Now().Sub(w.start).Milliseconds()
+	w.Op.SetMetricPercentile(fmt.Sprintf("%s_response_time_ms", w.label), float64(totalTime))
+	rate := float64(w.written/int(totalTime)) / 125 // bytes/ms -> mbps
+	w.Op.SetMetricPercentile(fmt.Sprintf("%s_response_rate_mbps", w.label), rate)
+	w.Op.SetMetricSum(fmt.Sprintf("%s_bytes", w.label), float64(w.written))
 	w.Op.End()
 }
 
@@ -45,7 +46,7 @@ func (w *InstrumentedResponseWriter) Write(p []byte) (n int, err error) {
 	written, err := w.ResponseWriter.Write(p)
 
 	if written > 0 && w.written == 0 {
-		w.Op.SetMetricPercentile(fmt.Sprintf("%s_first_byte", w.label), timeBeforeWrite.Sub(w.start).Seconds())
+		w.Op.SetMetricPercentile(fmt.Sprintf("%s_first_byte_ms", w.label), float64(timeBeforeWrite.Sub(w.start).Milliseconds()))
 	}
 
 	w.written += written
