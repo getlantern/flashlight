fffcf99b5be3631d6a6315ec78b7b6493ca9938a
Maxb <bittmanmax@gmail.com>
2020-05-07 18:10:53 -0700

    Fix broken comparator logic in integration test

--- desktop/integration_test.go
+++ desktop/integration_test.go
@@ -190,7 +190,7 @@ func TestProxying(t *testing.T) {
 			missingOps = make([]string, 0)
 			opsMx.RLock()
 			for _, op := range borda.FullyReportedOps {
-				if op == "report_issue" || op == "sysproxy_off" || op == "sysproxy_off_force" || op == "sysproxy_clear" || op == "probe" || op == "proxy_rank" || op == "proxy_selection_stability" || "replica_upload" || "replica_view" {
+				if op == "report_issue" || op == "sysproxy_off" || op == "sysproxy_off_force" || op == "sysproxy_clear" || op == "probe" || op == "proxy_rank" || op == "proxy_selection_stability" || op == "replica_upload" || op == "replica_view" {
 					// ignore these, as we don't do them (reliably) during the integration test
 					continue
 				}
