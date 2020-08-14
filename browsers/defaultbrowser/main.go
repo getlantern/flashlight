c08da7561afbc0a81f3999b2e317106c85dd6437
Harry Harpham <harry@getlantern.org>
2020-08-14 17:28:54 -0600

    Add browsers package

---
+++ browsers/defaultbrowser/main.go
@@ -0,0 +1,31 @@
+// Command defaultbrowser prints the name of the system's default web browser. It is difficult to
+// write meaningful unit tests for something like this, but this utility at least lets us do some
+// manual spot checking.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/getlantern/flashlight/browsers"
+)
+
+var timeout = flag.Duration("timeout", 5*time.Second, "")
+
+func main() {
+	flag.Parse()
+
+	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
+	defer cancel()
+
+	start := time.Now()
+	b, err := browsers.SystemDefault(ctx)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed:", err)
+		os.Exit(1)
+	}
+	fmt.Printf("The default browser is %v.\nThis operation took %v\n", b, time.Now().Sub(start))
+}
