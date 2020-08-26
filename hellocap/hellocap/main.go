e1255e942b2ce5cd6d7d0552ab6273caeeae052a
Harry Harpham <harry@getlantern.org>
2020-08-26 17:54:04 -0600

    Print browser name in hellocap command

--- hellocap/hellocap/main.go
+++ hellocap/hellocap/main.go
@@ -8,18 +8,29 @@ import (
 	"os"
 	"time"
 
+	"github.com/getlantern/flashlight/browsers"
 	"github.com/getlantern/flashlight/hellocap"
 )
 
 var timeout = flag.Duration("timeout", 10*time.Second, "")
 
 func main() {
+	flag.Parse()
+
 	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
 	defer cancel()
+
+	b, err := browsers.SystemDefault(ctx)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failed to obtain default browser:", err)
+		os.Exit(1)
+	}
+	fmt.Fprintln(os.Stderr, "Default browser identified as", b)
 
 	hello, err := hellocap.GetDefaultBrowserHello(ctx)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
 	}
 	fmt.Println(string(hello))
 }
