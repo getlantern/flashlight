757e0e62fa7ad863ca7aa6bc521a6f6344c4cd14
Myles Horton <myles@getlantern.org>
2020-10-08 16:59:30 -0400

    tweaked urls

--- common/beamconfig.go
+++ common/beamconfig.go
@@ -9,10 +9,10 @@ const (
 	TrackingID = "UA-21815217-23"
 
 	// GlobalURL URL for fetching the global config.
-	GlobalURL = "https://global.ss7hc6jm.io/global.yaml.gz"
+	GlobalURL = "https://globalconfig.ss7hc6jm.io/global.yaml.gz"
 
 	// GlobalStagingURL is the URL for fetching the global config in a staging environment.
-	GlobalStagingURL = "https://global.ss7hc6jm.io/global.yaml.gz"
+	GlobalStagingURL = "https://globalconfig.ss7hc6jm.io/global.yaml.gz"
 
 	// The following are over HTTP because proxies do not forward X-Forwarded-For
 	// with HTTPS and because we only support falling back to direct domain
