df9434e98281b883795787f5bde7d1be5dc68bac
Myles Horton <myles@getlantern.org>
2020-10-21 13:28:46 -0400

    fixed test

--- pro/proxy_test.go
+++ pro/proxy_test.go
@@ -38,11 +38,12 @@ func TestProxy(t *testing.T) {
 		return
 	}
 
-	req.Header.Set("Origin", "a.com")
+	origin := "http://localhost:48933"
+	req.Header.Set("Origin", origin)
 	resp, err := (&http.Client{}).Do(req)
 	if assert.NoError(t, err, "OPTIONS request should succeed") {
 		assert.Equal(t, 200, resp.StatusCode, "should respond 200 to OPTIONS")
-		assert.Equal(t, "a.com", resp.Header.Get("Access-Control-Allow-Origin"), "should respond with correct header")
+		assert.Equal(t, origin, resp.Header.Get("Access-Control-Allow-Origin"), "should respond with correct header")
 		_ = resp.Body.Close()
 	}
 	assert.Nil(t, m.Req, "should not pass the OPTIONS request to origin server")
@@ -51,18 +52,18 @@ func TestProxy(t *testing.T) {
 	if !assert.NoError(t, err) {
 		return
 	}
-	req.Header.Set("Origin", "a.com")
+	req.Header.Set("Origin", origin)
 	resp, err = (&http.Client{}).Do(req)
 	if assert.NoError(t, err, "GET request should have no error") {
 		assert.Equal(t, 200, resp.StatusCode, "should respond 200 ok")
-		assert.Equal(t, "a.com", resp.Header.Get("Access-Control-Allow-Origin"), "should respond with correct header")
+		assert.Equal(t, origin, resp.Header.Get("Access-Control-Allow-Origin"), "should respond with correct header")
 		msg, _ := ioutil.ReadAll(resp.Body)
 		_ = resp.Body.Close()
 		assert.Equal(t, "GOOD", string(msg), "should respond expected body")
 	}
 	if assert.NotNil(t, m.Req, "should pass through non-OPTIONS requests to origin server") {
 		t.Log(m.Req)
-		assert.Empty(t, m.Req.Header.Get("Origin"), "should strip off Origin header")
+		assert.Equal(t, origin, resp.Header.Get("Access-Control-Allow-Origin"), "should respond with correct header")
 	}
 
 	url = fmt.Sprintf("http://%s/pro/user-data", addr)
@@ -97,5 +98,4 @@ func TestProxy(t *testing.T) {
 	if assert.True(t, found) {
 		assert.Equal(t, "b@b.com", user.Email, "should store user data implicitly if response is gzipped JSON")
 	}
-
 }
