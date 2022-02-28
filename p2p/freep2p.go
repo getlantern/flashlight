1a6492e84f6eb9fa182f6142a7416d247fa5608c
Alec Soltzen <83119415+soltzen@users.noreply.github.com>
2022-02-28 10:08:08 +0100

    [p2p:freep2p] Use QuicReverseProxy

--- p2p/freep2p.go
+++ p2p/freep2p.go
@@ -4,27 +4,37 @@ import (
 	"context"
 	"encoding/hex"
 	"net"
-	"net/http"
+	"strconv"
 	"sync"
 
 	"github.com/anacrolix/dht/v2"
-	"github.com/elazarl/goproxy"
+	"github.com/getlantern/flashlight/quicproxy"
+	"github.com/getlantern/flashlight/upnp"
 )
 
 type FreeP2pCtx struct {
-	dhtServer   *dht.Server
-	infohashes  [][20]byte
-	addr        *net.TCPAddr
-	p2pFuncs    ReplicaP2pFunctions
-	proxyServer *http.Server
-	closeOnce   sync.Once
-	closeChan   chan struct{}
+	UpnpClient         *upnp.Client
+	ReverseProxyServer *quicproxy.QuicReverseProxy
+
+	dhtServer  *dht.Server
+	infohashes [][20]byte
+	addr       *net.TCPAddr
+	p2pFuncs   ReplicaP2pFunctions
+	closeOnce  sync.Once
+	closeChan  chan struct{}
 }
 
 func NewFreeP2pCtx(
 	peerInfoHashes []string,
 	p2pFuncs ReplicaP2pFunctions,
 ) (*FreeP2pCtx, error) {
+	if peerInfoHashes == nil || len(peerInfoHashes) == 0 {
+		return nil, log.Errorf("peerInfoHashes is nil or empty")
+	}
+	if p2pFuncs == nil {
+		return nil, log.Errorf("p2pFuncs is nil")
+	}
+
 	cfg := dht.NewDefaultServerConfig()
 	cfg.NoSecurity = false
 	s, err := dht.NewServer(cfg)
@@ -49,35 +59,41 @@ func NewFreeP2pCtx(
 		infohashes: ihs,
 		p2pFuncs:   p2pFuncs,
 		closeChan:  make(chan struct{}),
+		UpnpClient: upnp.New(),
 	}, nil
 }
 
-func (p2pCtx *FreeP2pCtx) StartConnectProxy(errChan chan error) (int, error) {
+func (p2pCtx *FreeP2pCtx) StartReverseProxy(
+	port int,
+	pemEncodedCert, pemEncodedPrivKey []byte,
+	errChan chan error,
+	verbose bool) error {
 	log.Debugf("Free peer: Initializing CONNECT proxy...")
-	ln, err := net.Listen("tcp", ":0")
+
+	p, err := quicproxy.NewReverseProxy(
+		":"+strconv.Itoa(port), // addr
+		pemEncodedCert,
+		pemEncodedPrivKey,
+		verbose,
+		errChan,
+	)
 	if err != nil {
-		return 0, log.Errorf("starting p2p CONNECT proxy: %v", err)
+		return log.Errorf("while NewReverseProxy %v", err)
 	}
-	proxy := goproxy.NewProxyHttpServer()
-	// proxy.Verbose = true
-	go func() {
-		log.Debugf("Free peer: Starting p2p CONNECT proxy on %s", ln.Addr())
-		p2pCtx.proxyServer = &http.Server{Addr: ln.Addr().String(), Handler: proxy}
-		err = p2pCtx.proxyServer.Serve(ln)
-		if err != nil && err != http.ErrServerClosed && errChan != nil {
-			errChan <- log.Errorf("P2p CONNECT proxy server failed: %v", err)
-		}
-		close(errChan)
-	}()
-	return ln.Addr().(*net.TCPAddr).Port, nil
+	p2pCtx.ReverseProxyServer = p
+	return nil
 }
 
-func (p2pCtx *FreeP2pCtx) Announce(port int) error {
-	log.Debugf("Free peer: Announcing %+v to port %v", p2pCtx.infohashes, port)
+func (p2pCtx *FreeP2pCtx) Announce() error {
+	if p2pCtx.ReverseProxyServer.Port == 0 {
+		return log.Errorf("Reverse proxy port is 0. Cannot proceed")
+	}
+	log.Debugf("Free peer: Announcing %+v to port %v",
+		p2pCtx.infohashes, p2pCtx.ReverseProxyServer.Port)
 	return p2pCtx.p2pFuncs.Announce(
 		[]*dht.Server{p2pCtx.dhtServer},
 		p2pCtx.infohashes,
-		port)
+		p2pCtx.ReverseProxyServer.Port)
 }
 
 // Close shutsdown this peer's resources.
@@ -87,10 +103,10 @@ func (p2pCtx *FreeP2pCtx) Close(ctx context.Context) {
 		if p2pCtx.dhtServer != nil {
 			p2pCtx.dhtServer.Close()
 		}
-		if p2pCtx.proxyServer != nil {
-			err := p2pCtx.proxyServer.Shutdown(ctx)
+		if p2pCtx.ReverseProxyServer != nil {
+			err := p2pCtx.ReverseProxyServer.Shutdown(ctx)
 			if err != nil {
-				log.Debugf("Error while closing proxy server: %v", err)
+				log.Debugf("Error while closing reverse proxy server: %v", err)
 			}
 		}
 		close(p2pCtx.closeChan)
