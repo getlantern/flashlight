6861c19509aa08bc16ecadaff46302554e3c1add
Myles Horton <myles@getlantern.org>
2022-06-23 15:36:24 -0400

    Added randomized POST body lengths

--- bypass/bypass.go
+++ bypass/bypass.go
@@ -2,11 +2,14 @@ package bypass
 
 import (
 	"bytes"
+	"crypto/rand"
 	"encoding/json"
-	"math/rand"
+	"math/big"
 	"net/http"
 	"sync"
 	"time"
+
+	mrand "math/rand"
 
 	"github.com/getlantern/flashlight"
 	"github.com/getlantern/flashlight/chained"
@@ -18,13 +21,15 @@ var log = golog.LoggerFor("bypass")
 type bypass struct {
 	client    *http.Client
 	infos     map[string]*chained.ChainedServerInfo
-	proxies   map[string]*proxy
+	proxies   []*proxy
 	mxProxies sync.Mutex
 }
 
 type proxy struct {
-	info *chained.ChainedServerInfo
-	done chan bool
+	*chained.ChainedServerInfo
+	name       string
+	done       chan bool
+	randString string
 }
 
 func (b *bypass) OnProxies(infos map[string]*chained.ChainedServerInfo) {
@@ -32,17 +37,39 @@ func (b *bypass) OnProxies(infos map[string]*chained.ChainedServerInfo) {
 	defer b.mxProxies.Unlock()
 	b.reset()
 	for k, v := range infos {
-		p := &proxy{info: v, done: make(chan bool)}
-		b.proxies[k] = p
+		p := b.newProxy(k, v)
+		b.proxies = append(b.proxies, p)
 		go p.start(b.client)
 	}
+}
+
+func (b *bypass) newProxy(name string, info *chained.ChainedServerInfo) *proxy {
+	return &proxy{
+		ChainedServerInfo: info,
+		name:              name,
+		done:              make(chan bool),
+	}
+}
+
+func randomizedString() string {
+	const charset = "abcdefghijklmnopqrstuvwxyz"
+	size, err := rand.Int(rand.Reader, big.NewInt(300))
+	if err != nil {
+		return ""
+	}
+
+	bytes := make([]byte, size.Int64())
+	for i := range bytes {
+		bytes[i] = charset[mrand.Intn(len(charset))]
+	}
+	return string(bytes)
 }
 
 func (b *bypass) reset() {
 	for _, v := range b.proxies {
 		v.stop()
 	}
-	b.proxies = make(map[string]*proxy)
+	b.proxies = make([]*proxy, 0)
 }
 
 // Starts client access to the bypass server. The client periodically sends traffic to the server both via
@@ -52,15 +79,19 @@ func Start(listen func(flashlight.ProxyListener)) {
 }
 
 func StartWith(client *http.Client, listen func(flashlight.ProxyListener)) {
-	b := &bypass{client: client, proxies: make(map[string]*proxy)}
+	b := &bypass{client: client, proxies: make([]*proxy, 0)}
 	listen(b)
 }
 
 func (p *proxy) start(client *http.Client) {
 	p.callRandomly(func() {
+		// We include a random length string here to make it harder for censors to identify lantern based on
+		// consistent packet lengths.
+		p.randString = randomizedString()
+
 		// Just posting all the info about the server allows us to control these fields fully on the server
 		// side.
-		infoJson, err := json.Marshal(p.info)
+		infoJson, err := json.Marshal(p)
 		if err != nil {
 			log.Errorf("Unable to marshal chained server info: %v", err)
 			return
@@ -81,7 +112,7 @@ func (p *proxy) callRandomly(f func()) {
 		select {
 		case <-p.done:
 			return
-		case <-time.After(90 + time.Duration(rand.Intn(60))*time.Second):
+		case <-time.After(90 + time.Duration(mrand.Intn(60))*time.Second):
 			f()
 		}
 	}
