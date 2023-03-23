bd847d3022e159428dfd3af3234ec7df753c0eaf
Noah Levenson <noahlevenson@gmail.com>
2023-03-23 13:29:28 -0400

    add basic impl

---
+++ chained/broflake_impl.go
@@ -0,0 +1,65 @@
+package chained
+
+import (
+	"context"
+	"net"
+	"net/http"
+
+	"github.com/getlantern/broflake/clientcore"
+	"github.com/getlantern/common/config"
+	"github.com/getlantern/flashlight/ops"
+)
+
+type broflakeImpl struct {
+	reportDialCore reportDialCoreFn // TODO: I don't know what this is for yet
+	t              *http.Transport
+	ui             *clientcore.UIImpl
+}
+
+func newBroflakeImpl(pc *config.ProxyConfig, reportDialCore reportDialCoreFn) (proxyImpl, error) {
+	// TODO: I don't know what the reportDialCoreFn is, and I'm not sure if I need to know. I'm
+	// just imitating the function signature and approach of other impls...
+
+	// TODO: extract config settings from ProxyConfig and override bo fields as applicable
+	bo := clientcore.NewDefaultBroflakeOptions()
+
+	// TODO: extract config settings from ProxyConfig and override wo fields as applicable
+	wo := clientcore.NewDefaultWebRTCOptions()
+
+	// TODO: here we need to inject our custom STUNBatch function as applicable, where should that code live?
+
+	// TODO: extract config settings from ProxyConfig and override these fields as applicable
+	qo := &clientcore.QUICLayerOptions{
+		ServerName:         "",
+		InsecureSkipVerify: true,
+	}
+
+	// Construct, init, and start a Broflake client!
+	bfconn, ui, err := clientcore.NewBroflake(bo, wo, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	ql, err := clientcore.NewQUICLayer(bfconn, qo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &broflakeImpl{
+		reportDialCore: reportDialCore,
+		t:              clientcore.CreateHTTPTransport(ql),
+		ui:             ui,
+	}, nil
+}
+
+func (b *broflakeImpl) dialServer(op *ops.Op, ctx context.Context) (net.Conn, error) {
+	// TODO: I don't know what to do with 'op'
+
+	// TODO: it may or may not be necessary to wrap this dial in a CONNECT, a la the "Integrate
+	// Broflake Redux" PR...
+	return b.t.DialContext(ctx, "whatever", "whatever")
+}
+
+func (b *broflakeImpl) close() {
+	b.ui.Stop()
+}
