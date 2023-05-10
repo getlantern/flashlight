0f8f90cf6562a04296cde7bff405fd099ccbb4a6
Jay <110402935+jay-418@users.noreply.github.com>
2023-05-09 20:40:06 -0500

    add issue_test, update ios/report_issue

--- ios/report_issue.go
+++ ios/report_issue.go
@@ -2,12 +2,13 @@ package ios
 
 import (
 	"bytes"
-	"context"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"time"
 
 	"github.com/getlantern/flashlight/email"
+	"github.com/getlantern/flashlight/issue"
 	"github.com/getlantern/flashlight/logging"
 	"github.com/getlantern/fronted"
 )
@@ -31,27 +32,76 @@ func init() {
 	}()
 }
 
-// ReportIssue reports an issue via email.
-func ReportIssue(isPro bool, userID int, proToken, deviceID, appVersion, deviceModel, iosVersion, emailAddress, issue, appLogsDir, tunnelLogsDir, proxiesYamlPath string) error {
+// // ReportIssueViaEmail reports an issue via email.
+// func ReportIssueViaEmail(isPro bool, userID int, proToken, deviceID, appVersion, deviceModel, iosVersion, emailAddress, issue, appLogsDir, tunnelLogsDir, proxiesYamlPath string) error {
+// 	proText := "no"
+// 	if isPro {
+// 		proText = "yes"
+// 	}
+// 	msg := &email.Message{
+// 		Template: "user-send-logs-ios",
+// 		From:     emailAddress,
+// 		Vars: map[string]interface{}{
+// 			"issue":        issue,
+// 			"userid":       userID,
+// 			"protoken":     proToken,
+// 			"prouser":      proText,
+// 			"deviceID":     deviceID,
+// 			"emailaddress": emailAddress,
+// 			"appversion":   appVersion,
+// 			"iosmodel":     deviceModel,
+// 			"iosversion":   iosVersion,
+// 		},
+// 	}
+// 	b := &bytes.Buffer{}
+// 	// 5MB is logfile size limit, and we have:
+// 	// two targets (app/netEx) with their own logs
+// 	// each target has 6 ios log files and 6 go log files
+// 	// for a total of 24 files * 5MB
+// 	_, err := logging.ZipLogFilesFrom(b, 5*1024*1024*24, 0, map[string]string{"app": appLogsDir, "tunnel": tunnelLogsDir})
+// 	if err != nil {
+// 		log.Errorf("Unable to zip log files: %v", err)
+// 	} else {
+// 		msg.Logs = b.Bytes()
+// 	}
+
+// 	bytes, err := ioutil.ReadFile(proxiesYamlPath)
+// 	if err != nil {
+// 		log.Errorf("Unable to read proxies.yaml for reporting issue: %v", err)
+// 	} else {
+// 		msg.Proxies = bytes
+// 	}
+
+// 	return email.Send(context.TODO(), msg)
+// }
+
+// reportIssueViaAPI reports an issue via the lantern-cloud/issue API
+func reportIssueViaAPI(
+	isPro bool,
+	userID int,
+	proToken,
+	deviceID,
+	appVersion,
+	deviceModel,
+	iosVersion,
+	emailAddress,
+	issueText,
+	appLogsDir,
+	tunnelLogsDir,
+	proxiesYamlPath string) error {
+
 	proText := "no"
 	if isPro {
 		proText = "yes"
 	}
-	msg := &email.Message{
-		Template: "user-send-logs-ios",
-		From:     emailAddress,
-		Vars: map[string]interface{}{
-			"issue":        issue,
-			"userid":       userID,
-			"protoken":     proToken,
-			"prouser":      proText,
-			"deviceID":     deviceID,
-			"emailaddress": emailAddress,
-			"appversion":   appVersion,
-			"iosmodel":     deviceModel,
-			"iosversion":   iosVersion,
-		},
-	}
+
+	// TODO depending on other platforms, this could be
+	description := fmt.Sprintf("iOS version: %v\nDevice model: %v\nDevice ID: %v", iosVersion, deviceModel, deviceID)
+
+	// make an empty slice of a slice of bytes
+	attachments := [][]byte{}
+
+	// attach app logs and tunnel logs
 	b := &bytes.Buffer{}
 	// 5MB is logfile size limit, and we have:
 	// two targets (app/netEx) with their own logs
@@ -61,15 +111,18 @@ func ReportIssue(isPro bool, userID int, proToken, deviceID, appVersion, deviceM
 	if err != nil {
 		log.Errorf("Unable to zip log files: %v", err)
 	} else {
-		msg.Logs = b.Bytes()
+		attachments = append(attachments, b.Bytes())
 	}
 
+	// attach proxies.yaml
 	bytes, err := ioutil.ReadFile(proxiesYamlPath)
 	if err != nil {
 		log.Errorf("Unable to read proxies.yaml for reporting issue: %v", err)
 	} else {
-		msg.Proxies = bytes
+		attachments = append(attachments, bytes)
 	}
 
-	return email.Send(context.TODO(), msg)
+	issue.SendIssueReport(issueText, "country-code", appVersion, proText, "iOS", description, emailAddress, attachments)
+
+	return err
 }
--- issue/issue.go
+++ issue/issue.go
@@ -1,59 +1,87 @@
 package issue
 
 import (
-	"bytes"
-	"encoding/base64"
-
-	"github.com/getlantern/flashlight/logging"
-	"github.com/getlantern/flashlight/util"
 	"github.com/getlantern/golog"
-	"github.com/getlantern/mandrill"
 )
 
 var (
-	log = golog.LoggerFor("issue")
+	log            = golog.LoggerFor("flashlight.issue")
+	maxLogTailSize = 1024768
 )
 
-const maxLogTailSize = 1024768
-
 // Attachment represents a single supported attachment
-type Attachment struct {
-	// the MIME type of the attachment
-	Type string `json:"type"`
-	// the file name of the attachment
-	Name string `json:"name"`
-	// the content of the attachment as a base64-encoded string
-	Content string `json:"content"`
-}
+// type Attachment struct {
+// 	// the MIME type of the attachment
+// 	Type string `json:"type"`
+// 	// the file name of the attachment
+// 	Name string `json:"name"`
+// 	// the content of the attachment as a base64-encoded string
+// 	Content string `json:"content"`
+// }
 
-type Report struct {
-	Type              string
-	CountryCode       string // ISO-3166-1
-	AppVersion        string // *.*.*
-	SubscriptionLevel string // free, pro, platnum
-	Platform          string
-	Description       string
-	UserEmail         string
-	attachments       []*Attachment
-}
+// type Report struct {
+// 	Type              string
+// 	CountryCode       string // ISO-3166-1
+// 	AppVersion        string // *.*.*
+// 	SubscriptionLevel string // free, pro, platnum
+// 	Platform          string
+// 	Description       string
+// 	UserEmail         string
+// 	attachments       []*Attachment
+// }
 
-func (r *Report) Send(maxLogSize string) {
-	if maxLogSize != "" {
-		if size, err := util.ParseFileSize(maxLogSize); err != nil {
-			log.Error(err)
-		} else {
-			log.Debug("Zipping log files for sending email")
-			buf := &bytes.Buffer{}
-			folder := "logs"
-			if _, err := logging.ZipLogFiles(buf, folder, size, int64(maxLogTailSize)); err == nil {
-				r.attachments = append(r.attachments, &mandrill.Attachment{
-					Type:    "application/zip",
-					Name:    folder + ".zip",
-					Content: base64.StdEncoding.EncodeToString(buf.Bytes()),
-				})
-			} else {
-				log.Errorf("Unable to zip log files: %v", err)
-			}
-		}
-	}
+func SendIssueReport(
+	issueType string,
+	countryCode string,
+	appVersion string,
+	subscriptionLevel string,
+	platform string,
+	description string,
+	userEmail string,
+	attachments [][]byte,
+) (err error) {
+
+	r := &Request{}
+
+	log.Debug("sending issue report")
+	r.Type = issueType
+	r.CountryCode = countryCode
+	r.AppVersion = appVersion
+	r.SubscriptionLevel = subscriptionLevel
+	r.Platform = platform
+	r.Description = description
+	r.UserEmail = userEmail
+
+	// log.Debug("zipping attachments for issue report")
+	// for _, attachment := range attachments {
+	// 	r.Attachments = append(r.Attachments, &Request_Attachment{
+	// 		Type:    "application/zip",
+	// 		Name:    "attachment",
+	// 		Content: attachment,
+	// 	})
+	// }
+
+	// // Zip attachments
+	// if maxLogSize != "" {
+	// 	if size, err := util.ParseFileSize(maxLogSize); err != nil {
+	// 		log.Error(err)
+	// 	} else {
+	// 		log.Debug("Zipping log files for sending email")
+	// 		buf := &bytes.Buffer{}
+	// 		folder := "logs"
+	// 		// attachmentfoo := make([]*issue.Attachment, 0)
+	// 		if _, err := logging.ZipLogFiles(buf, folder, size, int64(maxLogTailSize)); err == nil {
+	// 			r.Attachments = append(r.Attachments, &Request_Attachment{
+	// 				Type:    "application/zip",
+	// 				Name:    folder + ".zip",
+	// 				Content: buf.Bytes(),
+	// 			})
+	// 		} else {
+	// 			log.Errorf("Unable to zip log files: %v", err)
+	// 		}
+	// 	}
+	// }
+
+	// TODO send message to lantern-cloud
+	return nil
 }
