a781c81ffb2314a61152c46358edbf85cfaa4651
atavism <paul@getlantern.org>
2024-04-08 14:29:40 -0700

    Update pro client

---
+++ pro/http.go
@@ -0,0 +1,29 @@
+package pro
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/getlantern/flashlight/v7/proxied"
+)
+
+var (
+	httpClient = getHTTPClient(proxied.ParallelForIdempotent())
+)
+
+// GetHTTPClient creates a new http.Client that uses domain fronting and direct
+// proxies.
+func GetHTTPClient() *http.Client {
+	return httpClient
+}
+
+func getHTTPClient(rt http.RoundTripper) *http.Client {
+	return &http.Client{
+		Transport: rt,
+		// Don't follow redirects
+		CheckRedirect: func(req *http.Request, via []*http.Request) error {
+			return http.ErrUseLastResponse
+		},
+		Timeout: 30 * time.Second,
+	}
+}
