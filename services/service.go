7b8362a327823f7b476b27facfa1095205ec58e0
garmr <pdixon117@gmail.com>
2024-07-17 15:42:45 -0700

    specify interval/jitter instead of min/max waitSeconds

--- services/service.go
+++ services/service.go
@@ -8,40 +8,40 @@ import (
 	"github.com/getlantern/golog"
 )
 
-// callRandomly continuously calls fn on random intervals between minWaitSeconds and maxWaitSeconds,
-// until ctx is done, with the initial call being made immediately. fn can return a positive value
-// to extend the wait time.
+// callRandomly continuously calls fn randomly between interval-jitter and interval+jitter, with
+// the initial call being made immediately. fn can return a positive value to extend the wait time.
 func callRandomly(
 	fn func() int64,
-	minWaitSeconds int64,
-	maxWaitSeconds int64,
+	interval time.Duration,
+	jitter time.Duration,
 	done <-chan struct{},
 	logger golog.Logger,
 ) {
+	intervalMin := int64(interval - jitter)
+	intervalMax := int64(interval + jitter)
+
 	// calculate sleep time
-	sleep := func(extraSeconds int64, elapsed time.Duration) <-chan time.Time {
-		delay := mrand.Int63n(maxWaitSeconds) + minWaitSeconds + extraSeconds
-		delayDuration := time.Duration(delay)*time.Second + elapsed
+	sleep := func(extraDelay time.Duration) <-chan time.Time {
+		delay := mrand.Int63n(intervalMax) + intervalMin
+		delayDuration := time.Duration(delay) + extraDelay
 		logger.Debugf("Next run in %v", delayDuration)
 		return time.After(delayDuration)
 	}
 
-	var (
-		extraDelay int64
-		elapsed    time.Duration
-	)
-
 	// make initial call to fn immediately before entering loop
-	extraDelay = fn()
+	extraSeconds := fn()
 
+	extraDelay := time.Duration(extraSeconds) * time.Second
 	for {
 		select {
 		case <-done:
 			return
-		case <-sleep(extraDelay, elapsed):
+		case <-sleep(extraDelay):
 			start := time.Now()
-			extraDelay = fn()
-			elapsed = time.Since(start)
+			extraSeconds = fn()
+			elapsed := time.Since(start)
+			// aren't we supposed to subtract elapsed from extraDelay instead of adding?
+			extraDelay = time.Duration(extraSeconds)*time.Second + elapsed
 		}
 	}
 }
