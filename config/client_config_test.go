db046351c0a7ecb502cfd1c1f7037986b85c76e2
WendelHime <6754291+WendelHime@users.noreply.github.com>
2024-08-22 19:32:20 -0300

    chore: adding test case for checking if it's able to build provider with nil verifyHostname

--- config/client_config_test.go
+++ config/client_config_test.go
@@ -65,6 +65,50 @@ func TestFrontedProviders(t *testing.T) {
 				}, *provider1.Masquerades[0])
 			},
 		},
+		{
+			name: "client config with one provider should return a map with one provider and its config without verify hostname",
+			givenClientConfig: &ClientConfig{
+				Fronted: &FrontedConfig{
+					Providers: map[string]*ProviderConfig{
+						"provider1": {
+							HostAliases: map[string]string{
+								"host1": "alias1",
+							},
+							TestURL: "testURL",
+							Masquerades: []*fronted.Masquerade{
+								{
+									Domain:    "domain1",
+									IpAddress: "127.0.0.1",
+								},
+							},
+							Validator: &ValidatorConfig{
+								RejectStatus: []int{404},
+							},
+							PassthroughPatterns: []string{"pattern1"},
+							VerifyHostname:      nil,
+							FrontingSNIs: map[string]*fronted.SNIConfig{
+								"default": {
+									UseArbitrarySNIs: false,
+									ArbitrarySNIs:    []string{"sni1"},
+								},
+							},
+						},
+					},
+				},
+			},
+			assert: func(t *testing.T, providersMap map[string]*fronted.Provider) {
+				assert.Equal(t, 1, len(providersMap))
+				provider1 := providersMap["provider1"]
+				assert.Equal(t, map[string]string{"host1": "alias1"}, provider1.HostAliases)
+				assert.Equal(t, "testURL", provider1.TestURL)
+				assert.Equal(t, fronted.Masquerade{
+					Domain:         "domain1",
+					IpAddress:      "127.0.0.1",
+					VerifyHostname: nil,
+					SNI:            "sni1",
+				}, *provider1.Masquerades[0])
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
