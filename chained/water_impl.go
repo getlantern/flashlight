74be46da1aa9872f0b58b581e1cafb8a25476ee0
WendelHime <6754291+WendelHime@users.noreply.github.com>
2024-10-24 16:21:11 -0300

    fix: removing water_impl responsibility for setting the water version control dir

--- chained/water_impl.go
+++ chained/water_impl.go
@@ -8,7 +8,6 @@ import (
 	"log/slog"
 	"net"
 	"net/http"
-	"path/filepath"
 	"strings"
 	"time"
 
@@ -44,8 +43,7 @@ func newWaterImpl(dir, addr string, pc *config.ProxyConfig, reportDialCore repor
 	wasmAvailableAt := ptSetting(pc, "water_available_at")
 	transport := ptSetting(pc, "water_transport")
 	if wasm == nil && wasmAvailableAt != "" {
-		waterDir := filepath.Join(dir, "water")
-		vc, err := newWaterVersionControl(waterDir)
+		vc, err := newWaterVersionControl(dir)
 		if err != nil {
 			return nil, log.Errorf("failed to create version control: %w", err.Error())
 		}
