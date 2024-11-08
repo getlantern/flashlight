68770f93b3063465c7622be13567f18ebe7094a4
Adam Fisk <myles@getlantern.org>
2024-11-08 12:30:37 -0700

    Fixed test

--- dialer/bandit_test.go
+++ dialer/bandit_test.go
@@ -107,7 +107,7 @@ func TestNew(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := New(tt.opts)
+			got, err := NewBandit(tt.opts)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("New() error = %v, wantErr %v", err, tt.wantErr)
 				return
