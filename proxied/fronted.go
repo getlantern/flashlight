73af88cc9a82c46751a62d723d73ee786c021248
Adam Fisk <afisk@getlantern.org>
2024-12-12 12:57:48 -0700

    Ensure fronted is always initialized and self-contained in proxies package

--- proxied/fronted.go
+++ proxied/fronted.go
@@ -1,10 +1,28 @@
 package proxied
 
 import (
+	"crypto/x509"
 	"net/http"
+	"os"
+	"path/filepath"
 
+	"github.com/getlantern/flashlight/v7/common"
 	"github.com/getlantern/flashlight/v7/ops"
+	"github.com/getlantern/fronted"
 )
+
+var fronter fronted.Fronted = newFronted()
+
+func newFronted() fronted.Fronted {
+	var cacheFile string
+	dir, err := os.UserConfigDir()
+	if err != nil {
+		log.Errorf("Unable to get user config dir: %v", err)
+	} else {
+		cacheFile = filepath.Join(dir, common.DefaultAppName, "fronted_cache.json")
+	}
+	return fronted.NewFronted(cacheFile)
+}
 
 // Fronted creates an http.RoundTripper that proxies request using domain
 // fronting.
@@ -27,4 +45,9 @@ func (f frontedRoundTripper) RoundTrip(req *http.Request) (*http.Response, error
 		defer op.End()
 	}
 	return fronter.RoundTrip(req)
+}
+
+// OnNewFronts updates the fronted configuration with the new fronted providers.
+func OnNewFronts(pool *x509.CertPool, providers map[string]*fronted.Provider) {
+	fronter.OnNewFronts(pool, providers)
 }
