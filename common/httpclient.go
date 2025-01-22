189b344988c4c5a368ba48af464181378e90c6b3
Adam Fisk <afisk@getlantern.org>
2025-01-22 15:14:21 -0700

    Tweaks for using kindling

--- common/httpclient.go
+++ common/httpclient.go
@@ -10,6 +10,21 @@ import (
 var httpClient *http.Client
 var mutex = &sync.Mutex{}
 
+// These are the domains we will access via kindling.
+var domains = []string{
+	"api.iantem.io",
+	"df.iantem.io",
+	"iantem.io",
+	"raw.githubusercontent.com",
+	"media.githubusercontent.com",
+	"objects.githubusercontent.com",
+	"replica-r2.lantern.io",
+	"replica-search.lantern.io",
+	"update.getlantern.org",
+	"config.getiantem.org",
+	"globalconfig.flashlightproxy.com",
+}
+
 func GetHTTPClient() *http.Client {
 	mutex.Lock()
 	defer mutex.Unlock()
@@ -19,7 +34,7 @@ func GetHTTPClient() *http.Client {
 	// Set the client to the kindling client.
 	k := kindling.NewKindling(
 		kindling.WithDomainFronting("https://media.githubusercontent.com/media/getlantern/fronted/refs/heads/main/fronted.yaml.gz", ""),
-		kindling.WithProxyless("api.iantem.io", "iantem.io", "df.iantem.io"),
+		kindling.WithProxyless(domains...),
 	)
 	httpClient = k.NewHTTPClient()
 	return httpClient
