df65122d321a11e8395450d395e99b69c37fa7fe
Myles Horton <afisk@getlantern.org>
2025-06-16 11:57:19 -0600

    Add otel instrumentation to proxyless dials (#1502)
    * Add otel instrumentation to proxyless dials

--- dialer/proxyless_test.go
+++ dialer/proxyless_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 
 	"github.com/Jigsaw-Code/outline-sdk/transport"
+	"github.com/getlantern/flashlight/v7/ops"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -22,7 +23,6 @@ func (m *mockStreamDialer) DialStream(ctx context.Context, addr string) (transpo
 func TestGetOrCreateDialer_ExistingDialer(t *testing.T) {
 	// Arrange
 	host := "example.com"
-	addr := "example.com:443"
 	mockDialer := &mockStreamDialer{
 		dialStreamFunc: func(ctx context.Context, addr string) (transport.StreamConn, error) {
 			return nil, nil
@@ -34,7 +34,7 @@ func TestGetOrCreateDialer_ExistingDialer(t *testing.T) {
 	d := &proxylessDialer{}
 
 	// Act
-	dialer, err := d.getOrCreateDialer(context.Background(), host, addr)
+	dialer, err := d.getOrCreateDialer(context.Background(), host, ops.Begin("test_op"))
 
 	// Assert
 	assert.NoError(t, err)
@@ -44,7 +44,6 @@ func TestGetOrCreateDialer_ExistingDialer(t *testing.T) {
 func TestGetOrCreateDialer_NewDialerSuccess(t *testing.T) {
 	// Arrange
 	host := "example.com"
-	addr := "example.com:443"
 	mockDialer := &mockStreamDialer{}
 	d := &proxylessDialer{
 		newDialer: func(ctx context.Context, testDomains []string, configBytes []byte) (transport.StreamDialer, error) {
@@ -54,7 +53,7 @@ func TestGetOrCreateDialer_NewDialerSuccess(t *testing.T) {
 	}
 
 	// Act
-	dialer, err := d.getOrCreateDialer(context.Background(), host, addr)
+	dialer, err := d.getOrCreateDialer(context.Background(), host, ops.Begin("test_op"))
 
 	// Assert
 	assert.NoError(t, err)
@@ -64,7 +63,6 @@ func TestGetOrCreateDialer_NewDialerSuccess(t *testing.T) {
 func TestGetOrCreateDialer_NewDialerFailure(t *testing.T) {
 	// Arrange
 	host := "example.com"
-	addr := "example.com:443"
 	expectedErr := errors.New("failed to create dialer")
 	d := &proxylessDialer{
 		newDialer: func(ctx context.Context, testDomains []string, configBytes []byte) (transport.StreamDialer, error) {
@@ -74,7 +72,7 @@ func TestGetOrCreateDialer_NewDialerFailure(t *testing.T) {
 	}
 
 	// Act
-	dialer, err := d.getOrCreateDialer(context.Background(), host, addr)
+	dialer, err := d.getOrCreateDialer(context.Background(), host, ops.Begin("test_op"))
 
 	// Assert
 	assert.Nil(t, dialer)
@@ -123,7 +121,7 @@ func TestDialContext_CreateDialerError(t *testing.T) {
 
 	// Assert
 	assert.Nil(t, conn)
-	assert.EqualError(t, err, "failed to create smart dialer: failed to create dialer")
+	assert.Error(t, err)
 }
 
 func TestDialContext_DialStreamError(t *testing.T) {
@@ -148,5 +146,5 @@ func TestDialContext_DialStreamError(t *testing.T) {
 
 	// Assert
 	assert.Nil(t, conn)
-	assert.EqualError(t, err, "failed to dial stream with new dialer: failed to dial stream")
+	assert.Error(t, err)
 }
